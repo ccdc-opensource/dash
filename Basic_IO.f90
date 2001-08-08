12e62e32bd740b8b9d9589ea846acf0d928ad74f
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2001-08-08 17:02:19 +0000

    Wrapper routines for Winteracter routines

--- Basic_IO.f90
+++ Basic_IO.f90
@@ -8,12 +8,30 @@
 !
       USE WINTERACTER
       
-      CHARACTER*(*), INTENT(IN) :: TheMessage
+      CHARACTER*(*), INTENT(IN   ) :: TheMessage
 
       CALL WMessageBox(OkOnly,ExclamationIcon,CommonOk,TheMessage(1:LEN_TRIM(TheMessage)),"Error")
       RETURN
 
       END SUBROUTINE ErrorMessage
+!
+!*****************************************************************************
+!
+      SUBROUTINE DebugErrorMessage(TheMessage)
+!
+! Displays an error message that should be ignored in the release version of DASH.
+! Before release, this subroutine should simply RETURN without doing anything.
+!
+! INPUT   : TheMessage : The message to be displayed
+!
+      USE WINTERACTER
+      
+      CHARACTER*(*), INTENT(IN   ) :: TheMessage
+
+      CALL WMessageBox(OkOnly,ExclamationIcon,CommonOk,TheMessage(1:LEN_TRIM(TheMessage)),"Debug Error")
+      RETURN
+
+      END SUBROUTINE DebugErrorMessage
 !
 !*****************************************************************************
 !
@@ -37,6 +55,161 @@
       RETURN
 
       END FUNCTION Confirm
+!
+!*****************************************************************************
+!
+      SUBROUTINE GetEvent
+!
+! Fetches the last event from Winteracter and places it in two global variables
+! called EventType and EventInfo (both in VARIABLES)
+!
+      USE WINTERACTER
+      USE DRUID_HEADER
+      USE VARIABLES
+
+      IMPLICIT NONE
+
+  10  CALL WMessage(EventType,EventInfo)
+      SELECT CASE (EventInfo%WIN)
+        CASE (0) ! Main window
+          SELECT CASE (EventType)
+!            CASE (Expose,Resize)
+!              CALL Redraw()
+!              GOTO 10
+            CASE (CloseRequest)
+              CALL WExit
+              GOTO 10
+          END SELECT
+        CASE (IDD_Plot_Option_Dialog)
+          CALL DealWithPlotOptionsWindow
+          GOTO 10
+        CASE (IDD_Structural_Information)
+          CALL DealWithStructuralInformation
+          GOTO 10
+        CASE (IDD_Data_Properties)
+          CALL DealWithDiffractionSetupPane
+          GOTO 10
+        CASE (IDD_Crystal_Symmetry)
+          CALL DealWithCrystalSymmetryPane
+          GOTO 10
+      END SELECT
+
+      END SUBROUTINE GetEvent
+!
+!*****************************************************************************
+!
+      SUBROUTINE PeekEvent
+!
+! Reports if events are waiting to be handled. If not, program execution resumes.
+! If an event is waiting, it is placed in two global variables
+! called EventType and EventInfo (both in VARIABLES)
+!
+      USE WINTERACTER
+      USE DRUID_HEADER
+      USE VARIABLES
+
+      IMPLICIT NONE
+
+  10  CALL WMessagePeek(EventType,EventInfo)
+      IF (EventType .NE. NoMessage) THEN
+        SELECT CASE (EventInfo%WIN)
+          CASE (0) ! Main window
+            SELECT CASE (EventType)
+!              CASE (Expose,Resize)
+!                CALL Redraw()
+!                GOTO 10
+              CASE (CloseRequest)
+                CALL WExit
+                GOTO 10
+            END SELECT
+          CASE (IDD_Plot_Option_Dialog)
+            CALL DealWithPlotOptionsWindow
+            GOTO 10
+          CASE (IDD_Structural_Information)
+            CALL DealWithStructuralInformation
+            GOTO 10
+          CASE (IDD_Data_Properties)
+            CALL DealWithDiffractionSetupPane
+            GOTO 10
+          CASE (IDD_Crystal_Symmetry)
+            CALL DealWithCrystalSymmetryPane
+            GOTO 10
+        END SELECT
+      ENDIF
+
+      END SUBROUTINE PeekEvent
+!
+!*****************************************************************************
+!
+      SUBROUTINE PushActiveWindowID
+!
+! In order for a Winteracter window to respond, it must be 'selected' first.
+! Only a single Winteracter window can be 'selected' at a time.
+! This routine pushes the ID of the currently selected window onto a small stack.
+!
+! NOTE: as with all stack mechanisms, each call to PushActiveWindowID must be
+! matched with a call to PopActiveWindowID
+!
+      USE WINTERACTER
+
+      IMPLICIT NONE
+
+      INTEGER MaxWinStack
+      PARAMETER (MaxWinStack = 25)
+  
+      INTEGER WinStackPtr
+      DATA    WinStackPtr / MaxWinStack /
+      INTEGER WinStack
+    
+      COMMON /COMWS/ WinStackPtr, WinStack(1:MaxWinStack)
+
+! Check if stack full
+      IF (WinStackPtr .EQ. 0) RETURN
+! If not, store current window ID
+      WinStack(WinStackPtr) = WInfoDialog(CurrentDialog)
+! Dec(StackPtr)
+      WinStackPtr = WinStackPtr - 1
+      RETURN
+
+      END SUBROUTINE PushActiveWindowID
+!
+!*****************************************************************************
+!
+      SUBROUTINE PopActiveWindowID
+!
+! In order for a Winteracter window to respond, it must be 'selected' first.
+! Only a single Winteracter window can be 'selected' at a time.
+! This routine pops the ID of the previously selected window from a small stack.
+!
+! NOTE: as with all stack mechanisms, each call to PopActiveWindowID must
+! match with a call to PushActiveWindowID
+!
+      USE WINTERACTER
+
+      IMPLICIT NONE
+
+      INTEGER MaxWinStack
+      PARAMETER (MaxWinStack = 25)
+
+      INTEGER WinStackPtr
+      DATA    WinStackPtr / MaxWinStack /
+      INTEGER WinStack
+
+      COMMON /COMWS/ WinStackPtr, WinStack(1:MaxWinStack)
+
+! Check if stack empty
+      IF (WinStackPtr .EQ. 25) THEN
+        CALL ErrorMessage('Serious programming bug: WinStack empty.')
+        RETURN
+      ENDIF
+! If not, Inc(StackPtr)
+      WinStackPtr = WinStackPtr + 1
+! Restore current window ID
+!O      IF (WinStack(WinStackPtr) .NE. 0) CALL WDialogSelect(WinStack(WinStackPtr))
+      CALL WDialogSelect(WinStack(WinStackPtr))
+      RETURN
+
+      END SUBROUTINE PopActiveWindowID
 !
 !*****************************************************************************
 !
