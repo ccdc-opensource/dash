fb95ce7bf531b33da51b662782e6b1ba16a06c3c
Elna Pidcock <pidcock@ccdc.cam.ac.uk>
2001-08-08 14:30:15 +0000

    EP appended. Uploading information to SA_Summary grid.

--- MultiRun.f90
+++ MultiRun.f90
@@ -28,8 +28,11 @@
 
 	RESTART = (MaxRuns .GT. 1 .AND. ChiMult .GT. 0.0)
 
-    CALL WDialogSelect(IDD_SA_Multi_Completed)
-    CALL WDialogClearField(IDF_SA_Solution_Grid)
+!ep    CALL WDialogSelect(IDD_SA_Multi_Completed)
+!    CALL WDialogClearField(IDF_SA_Solution_Grid)
+	
+	CALL WDialogSelect(IDD_SA_Multi_completed_ep)
+    CALL WDialogClearField(IDF_SA_Summary)
 
 
     CALL WDialogSelect(IDD_SA_Action1)
@@ -67,7 +70,7 @@
 	INTEGER SA_Run_Number
 	COMMON /MULRUN/ RESTART, SA_Run_Number, MaxRuns, MinMoves, MaxMoves, ChiMult
 
-    REAL Grid_ProfileChi, Grid_IntesityChi
+    REAL Grid_ProfileChi, Grid_IntensityChi
 	CHARACTER*255 Grid_Buffer
     CHARACTER*80 SA_Label
 	CHARACTER*4 CNruns,CMruns
@@ -76,27 +79,42 @@
 	ICurSel = WinfoDialog(CurrentDialog)
 
 
-    CALL WDialogSelect(IDD_SA_Multi_Completed)
-    CALL WGridRows(IDF_SA_Solution_Grid, SA_Run_Number)
+!ep    CALL WDialogSelect(IDD_SA_Multi_Completed)
+!ep    CALL WGridRows(IDF_SA_Solution_Grid, SA_Run_Number)
     
+	CALL WDialogSelect(IDD_SA_Multi_completed_ep)
+    CALL WGridRows(IDF_SA_Summary, SA_Run_Number)
+
     DO I = SA_Run_Number - 1,1,-1
-		CALL WGridGetCellReal(IDF_SA_Solution_Grid,3,I,Grid_ProfileChi)
+!ep		CALL WGridGetCellReal(IDF_SA_Solution_Grid,3,I,Grid_ProfileChi)
+!		IF (Grid_ProfileChi .GT. ProfileChiSquared) THEN
+!			CALL WGridGetCellString(IDF_SA_Solution_Grid,1,I,Grid_Buffer)
+!			CALL WGridGetCellReal(IDF_SA_Solution_Grid,2,I,Grid_IntensityChi)
+
+!		    CALL WGridPutCellReal(IDF_SA_Solution_Grid,3,I+1,Grid_ProfileChi,'(f7.2)')
+!		    CALL WGridPutCellReal(IDF_SA_Solution_Grid,2,I+1,Grid_IntensityChi,'(f7.2)')
+!			CALL WGridPutCellString(IDF_SA_Solution_Grid,1,I+1,Grid_Buffer)
+
+
+		CALL WGridGetCellReal(IDF_SA_Summary,3,I,Grid_ProfileChi)
 		IF (Grid_ProfileChi .GT. ProfileChiSquared) THEN
-			CALL WGridGetCellString(IDF_SA_Solution_Grid,1,I,Grid_Buffer)
-			CALL WGridGetCellReal(IDF_SA_Solution_Grid,2,I,Grid_IntensityChi)
-
-		    CALL WGridPutCellReal(IDF_SA_Solution_Grid,3,I+1,Grid_ProfileChi,'(f7.2)')
-		    CALL WGridPutCellReal(IDF_SA_Solution_Grid,2,I+1,Grid_IntensityChi,'(f7.2)')
-			CALL WGridPutCellString(IDF_SA_Solution_Grid,1,I+1,Grid_Buffer)
-
+			CALL WGridGetCellString(IDF_SA_Summary,1,I,Grid_Buffer)
+	CALL WGridGetCellReal(IDF_SA_Summary,4,I,Grid_IntensityChi)
+			CALL WGridPutCellReal(IDF_SA_Summary,3,I+1,Grid_ProfileChi,'(f7.2)')
+		    CALL WGridPutCellReal(IDF_SA_Summary,4,I+1,Grid_IntensityChi,'(f7.2)')
+			CALL WGridPutCellString(IDF_SA_Summary,1,I+1,Grid_Buffer)	
 		ELSE
 		    EXIT
 		ENDIF
 	END DO
 
-	CALL WGridPutCellString(IDF_SA_Solution_Grid,1,I+1,PdbFileName(1:len_trim(PdbFileName)))
-	CALL WGridPutCellReal(IDF_SA_Solution_Grid,3,  I+1,ProfileChiSquared,'(f7.2)')
-	CALL WGridPutCellReal(IDF_SA_Solution_Grid,2,  I+1,IntensityChiSquared,'(f7.2)')
+!ep	CALL WGridPutCellString(IDF_SA_Solution_Grid,1,I+1,PdbFileName(1:len_trim(PdbFileName)))
+!	CALL WGridPutCellReal(IDF_SA_Solution_Grid,3,  I+1,ProfileChiSquared,'(f7.2)')
+!	CALL WGridPutCellReal(IDF_SA_Solution_Grid,2,  I+1,IntensityChiSquared,'(f7.2)')
+
+	CALL WGridPutCellString(IDF_SA_Summary,1,I+1,PdbFileName(1:len_trim(PdbFileName)))
+	CALL WGridPutCellReal(IDF_SA_Summary,3,  I+1,ProfileChiSquared,'(f7.2)')
+	CALL WGridPutCellReal(IDF_SA_Summary,4,  I+1,IntensityChiSquared,'(f7.2)')
 
     CALL WDialogSelect(IDD_SA_Action1)
 
@@ -122,28 +140,36 @@
 	SUBROUTINE SaveMultiRun_LogData
 	USE WINTERACTER
 	USE DRUID_HEADER
-    character*80 logsa_file,cssr_file,pdb_file,ccl_file,log_file
-    common /outfilnam/ logsa_file,cssr_file,pdb_file,ccl_file,log_file
-    common /outfillen/ logsa_flen,cssr_flen,pdb_flen,ccl_flen,log_flen
+! ep appended
+    character*80 logsa_file,cssr_file,pdb_file,ccl_file,log_file, pro_file
+    common /outfilnam/ logsa_file,cssr_file,pdb_file,ccl_file,log_file, pro_file
+    common /outfillen/ logsa_flen,cssr_flen,pdb_flen,ccl_flen,log_flen, pro_flen
 
 	LOGICAL RESTART
 	INTEGER SA_Run_Number, I
 	COMMON /MULRUN/ RESTART, SA_Run_Number, MaxRuns, MinMoves, MaxMoves, ChiMult
 
-    REAL Grid_ProfileChi, Grid_IntesityChi
+    REAL Grid_ProfileChi, Grid_IntensityChi
 	CHARACTER*255 Grid_Buffer
 
 	ICurSel = WinfoDialog(CurrentDialog)
 
-    CALL WDialogSelect(IDD_SA_Multi_Completed)
+!ep    CALL WDialogSelect(IDD_SA_Multi_Completed)
+	CALL WDialogSelect(IDD_SA_Multi_completed_ep)
 
 	OPEN(unit=101, file=log_file(1:log_flen), status = 'unknown', err = 99)
 	WRITE(101,*) 'File name,Profile Chi Squared,Intensity Chi Squared'
     DO I = 1, SA_Run_Number
-		CALL WGridGetCellReal(IDF_SA_Solution_Grid,3,I,Grid_ProfileChi)
-		CALL WGridGetCellString(IDF_SA_Solution_Grid,1,I,Grid_Buffer)
-		CALL WGridGetCellReal(IDF_SA_Solution_Grid,2,I,Grid_IntensityChi)
+!ep		CALL WGridGetCellReal(IDF_SA_Solution_Grid,3,I,Grid_ProfileChi)
+!		CALL WGridGetCellString(IDF_SA_Solution_Grid,1,I,Grid_Buffer)
+!		CALL WGridGetCellReal(IDF_SA_Solution_Grid,2,I,Grid_IntensityChi)
+!		WRITE(101,10) Grid_Buffer(1:len_trim(Grid_Buffer)),Grid_ProfileChi,Grid_IntensityChi
+
+		CALL WGridGetCellReal(IDF_SA_Summary,3,I,Grid_ProfileChi)
+		CALL WGridGetCellString(IDF_SA_Summary,1,I,Grid_Buffer)
+		CALL WGridGetCellReal(IDF_SA_Summary,4,I,Grid_IntensityChi)
 		WRITE(101,10) Grid_Buffer(1:len_trim(Grid_Buffer)),Grid_ProfileChi,Grid_IntensityChi
+
 	END DO
 
  10 FORMAT(A,',',F10.4,',',F10.4)
