e27caec97826ee6e17fb0487d1a7817842b2e714
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2001-09-17 14:04:53 +0000

    All .for are now .f90 and processed by SPAG

---
+++ DoAtomPositions.f90
@@ -0,0 +1,36 @@
+!*==DO_ATOM_POS.f90  processed by SPAG 6.11Dc at 13:14 on 17 Sep 2001
+!CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+!
+      SUBROUTINE DO_ATOM_POS(TRANS,ROTA,POS,NATOMS)
+!     -------------------------------------------------------
+!
+      REAL*8 TRANS(3), ROTA(3,3), POSIN(3), POS(3,NATOMS)
+!
+      DO J = 1, NATOMS
+        DO I = 1, 3
+          POSIN(I) = POS(I,J)
+        ENDDO
+        CALL ROTCAR(POSIN,POS(1,J),ROTA)
+        DO I = 1, 3
+          POS(I,J) = POS(I,J) + TRANS(I)
+        ENDDO
+      ENDDO
+!
+      END SUBROUTINE DO_ATOM_POS
+!*==ROTCAR.f90  processed by SPAG 6.11Dc at 13:14 on 17 Sep 2001
+!
+!
+!----------<<<<<<<<<<<<<<<==========+++++++++==========>>>>>>>>>>>>>>>----------
+!
+      SUBROUTINE ROTCAR(XORTO,XORTN,ROTA)
+      REAL*8 XORTO(3), XORTN(3), ROTA(3,3)
+!
+      DO I = 1, 3
+        XORTN(I) = 0.
+        DO J = 1, 3
+          XORTN(I) = XORTN(I) + ROTA(I,J)*XORTO(J)
+        ENDDO
+      ENDDO
+!
+      RETURN
+      END SUBROUTINE ROTCAR
