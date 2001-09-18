e34ca9087c91541e4a39a5e90d73c16ab2a2c770
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2001-09-18 17:05:11 +0000

    Deleted unused code (SPAG)

--- Fcn.f90
+++ Fcn.f90
@@ -25,30 +25,3 @@
 !
 !*****************************************************************************
 !
-      SUBROUTINE FCN_PRT(N,THETA,H)
-
-
-!
-      use winteracter
-      use druid_header
-!
-      DOUBLE PRECISION THETA(*), H
-
-      INCLUDE 'PARAMS.INC'
-
-      integer ioptb,iopta,ioptt,iz1,iz2,iz3
-      common /zmcomi/ ntatm,natoms(maxfrg),&
-      ioptb(maxatm,maxfrg),iopta(maxatm,maxfrg),ioptt(maxatm,maxfrg),&
-      iz1(maxatm,maxfrg),iz2(maxatm,maxfrg),iz3(maxatm,maxfrg)
-      common /zmcomg/ icomflg(maxfrg)
-
-      call makefrac_prt(theta,n,6)
-      call valchi(snglh)
-      h=dble(snglh)
-
-      RETURN
-
-      END
-!
-!*****************************************************************************
-!
