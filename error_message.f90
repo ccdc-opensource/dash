6c510ebcf2602475761c883f07f75370e95ce2ea
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2001-09-21 17:43:39 +0000

    Added revised version, but left old one untouched

--- error_message.f90
+++ error_message.f90
@@ -56,3 +56,78 @@
 	  if (aadd.ne.0.0) argmax(1)=armx
 !
 	END
+!
+!*****************************************************************************
+!
+      SUBROUTINE CHKMAXREF_2(PCXX)
+! Checks if the maximum number of reflections have been exceeded
+!
+      USE WINTERACTER
+
+      EXTERNAL PCXX
+
+      INCLUDE 'PARAMS.INC'
+      INCLUDE 'REFLNS.INC'
+
+      INTEGER         NPTS
+      REAL                  ZARGI,        ZOBS,        ZDOBS,        ZWT
+      INTEGER                                                                    ICODEZ
+      REAL                                                                                      KOBZ
+      COMMON /ZSTORE/ NPTS, ZARGI(MPPTS), ZOBS(MPPTS), ZDOBS(MPPTS), ZWT(MPPTS), ICODEZ(MPPTS), KOBZ(MPPTS)
+
+      COMMON /PRPKCN/ARGK,PKCNSP(6,9,5), KPCNSP(6,9,5),DTDPCN(6),DTDWL,&
+      NPKCSP(9,5),ARGMIN(5),ARGMAX(5), ARGSTP(5), PCON
+
+      INTEGER iorda(10)
+      REAL    ardif(10) ! Difference
+      REAL    aadd ! Add
+      REAL    arrt ! Relative
+
+      LOGICAL routine_called
+      SAVE routine_called
+      DATA routine_called / .FALSE. /
+!
+      aadd = 0.0
+      IF (maxk .GT. 360) THEN
+!.. We've too many reflections ... must reduce
+        IF (.NOT. routine_called) THEN
+          CALL WMessageBox(OKOnly,InformationIcon,CommonOK,  &
+               'DASH has a maximium limit of 350 reflections.'//CHAR(13)//&
+               'Only the 350 lowest angle reflections will be indexed and used','File truncation')
+          routine_called = .TRUE.
+        ENDIF
+        know = 350   ! know is a global variable used by PCXX
+        CALL PCXX(2) ! Changes argk
+        arrt = argk
+! JvdS I don't understand the following lines at all: it seems to search for the maximum _difference_
+! in 2 theta between two reflections between reflections 350 and 360. WHY!?
+!
+! JvdS was:
+!       DO II = 1, 1
+        DO II = 1, 10
+          know = 350 + II ! know is a global variable
+          CALL PCXX(2)
+          ardif(II) = argk - arrt  ! argk is a global variable
+          arrt = argk
+        END DO
+        CALL sortx(ardif,iorda,10)
+        maxk = 349 + iorda(10)
+        aadd = ardif(10)
+      END IF
+      know = maxk
+! Calculate peak centre of know in argk, and its derivatives
+      CALL pcxx(2)
+! argk already contains the peak position of the very very last reflection because
+! maxk has already been updated: why are we adding aadd again?
+      armx = argk + aadd
+      II = 1
+      DO WHILE ((zargi(II) .LT. armx) .AND. (II .LE. MPPTS))
+        II = II + 1
+      END DO
+      npts = MIN(npts,II)
+      IF (aadd .NE. 0.0) argmax(1) = armx
+
+      END SUBROUTINE CHKMAXREF_2
+!
+!*****************************************************************************
+!
