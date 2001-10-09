6fb2a43a08c6f4d6005e8598a8127cdf17f4750e
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2001-10-09 13:00:41 +0000

    Added calculation of 'crystallographic' centre of mass

--- create_fob.f90
+++ create_fob.f90
@@ -10,7 +10,6 @@
       REAL tiso, occ
       COMMON /zmcomo/ tiso(maxatm,maxfrg), occ(maxatm,maxfrg)
       DOUBLE PRECISION blen, alph, bet, f2cmat
-      CHARACTER*3 asym
       INTEGER ioptb, iopta, ioptt, iz1, iz2, iz3
 !
       COMMON /zmcomi/ ntatm, natoms(maxfrg), ioptb(maxatm,maxfrg),      &
@@ -19,7 +18,9 @@
      &                iz3(maxatm,maxfrg)
       COMMON /zmcomr/ blen(maxatm,maxfrg), alph(maxatm,maxfrg),         &
      &                bet(maxatm,maxfrg), f2cmat(3,3)
-      COMMON /zmcomc/ asym(maxatm,maxfrg)
+      CHARACTER*3     asym
+      CHARACTER*5                          OriginalLabel
+      COMMON /zmcomc/ asym(maxatm,maxfrg), OriginalLabel(maxatm,maxfrg)
 !
       COMMON /frgcom/ nfrag
 !
@@ -37,8 +38,6 @@
       REAL    ARGK
       REAL    DSTAR
       COMMON /PROFTIC/ NTIC,IH(3,MTIC),ARGK(MTIC),DSTAR(MTIC)
-
-
 
       LOGICAL HYDNOT
       COMMON /HIDDAT/ HYDNOT(150), nsatom, isatom(150)
@@ -84,21 +83,21 @@
       REAL a3(melem), b3(melem), a4(melem), b4(melem)
       REAL cv(melem)
 !
-      DATA SYMBA/'Du', 'H  ', 'He ', 'Li ', 'Be ', 'B  ', 'C  ', 'N  ', &
-     &     'O  ', 'F  ', 'Ne ', 'Na ', 'Mg ', 'Al ', 'Si ', 'P  ',      &
-     &     'S  ', 'Cl ', 'Ar ', 'K  ', 'Ca ', 'Sc ', 'Ti ', 'V  ',      &
-     &     'Cr ', 'Mn ', 'Fe ', 'Co ', 'Ni ', 'Cu ', 'Zn ', 'Ga ',      &
-     &     'Ge ', 'As ', 'Se ', 'Br ', 'Kr ', 'Rb ', 'Sr ', 'Y  ',      &
-     &     'Zr ', 'Nb ', 'Mo ', 'Tc ', 'Ru ', 'Rh ', 'Pd ', 'Ag ',      &
-     &     'Cd ', 'In ', 'Sn ', 'Sb ', 'Te ', 'I  ', 'Xe ', 'Cs ',      &
-     &     'Ba ', 'La ', 'Ce ', 'Pr ', 'Nd ', 'Pm ', 'Sm ', 'Eu ',      &
-     &     'Gd ', 'Tb ', 'Dy ', 'Ho ', 'Er ', 'Tm ', 'Yb ', 'Lu ',      &
-     &     'Hf ', 'Ta ', 'W  ', 'Re ', 'Os ', 'Ir ', 'Pt ', 'Au ',      &
-     &     'Hg ', 'Tl ', 'Pb ', 'Bi ', 'Po ', 'At ', 'Rn ', 'Fr ',      &
-     &     'Ra ', 'Ac ', 'Th ', 'Pa ', 'U  ', 'Np ', 'Pu ', 'Am ',      &
-     &     'Cm ', 'Bk ', 'Cf '/
+      DATA SYMBA/       'H  ', 'He ', 'Li ', 'Be ', 'B  ', 'C  ', 'N  ', &
+     &           'O  ', 'F  ', 'Ne ', 'Na ', 'Mg ', 'Al ', 'Si ', 'P  ',      &
+     &           'S  ', 'Cl ', 'Ar ', 'K  ', 'Ca ', 'Sc ', 'Ti ', 'V  ',      &
+     &           'Cr ', 'Mn ', 'Fe ', 'Co ', 'Ni ', 'Cu ', 'Zn ', 'Ga ',      &
+     &           'Ge ', 'As ', 'Se ', 'Br ', 'Kr ', 'Rb ', 'Sr ', 'Y  ',      &
+     &           'Zr ', 'Nb ', 'Mo ', 'Tc ', 'Ru ', 'Rh ', 'Pd ', 'Ag ',      &
+     &           'Cd ', 'In ', 'Sn ', 'Sb ', 'Te ', 'I  ', 'Xe ', 'Cs ',      &
+     &           'Ba ', 'La ', 'Ce ', 'Pr ', 'Nd ', 'Pm ', 'Sm ', 'Eu ',      &
+     &           'Gd ', 'Tb ', 'Dy ', 'Ho ', 'Er ', 'Tm ', 'Yb ', 'Lu ',      &
+     &           'Hf ', 'Ta ', 'W  ', 'Re ', 'Os ', 'Ir ', 'Pt ', 'Au ',      &
+     &           'Hg ', 'Tl ', 'Pb ', 'Bi ', 'Po ', 'At ', 'Rn ', 'Fr ',      &
+     &           'Ra ', 'Ac ', 'Th ', 'Pa ', 'U  ', 'Np ', 'Pu ', 'Am ',      &
+     &           'Cm ', 'Bk ', 'Cf ', 'Du '/
 !
-      DATA A1/0.0000, 0.48992, 0.87340, 1.12820, 1.59190, 2.05450,      &
+      DATA A1/0.48992, 0.87340, 1.12820, 1.59190, 2.05450,      &
      &     2.31000, 12.21260, 3.04850, 3.53920, 3.95530, 4.76260,       &
      &     5.42040, 6.42020, 6.29150, 6.43450, 6.90530, 11.46040,       &
      &     7.48450, 8.21860, 8.62660, 9.18900, 9.75950, 10.29710,       &
@@ -114,9 +113,9 @@
      &     27.00590, 16.88190, 20.68090, 27.54460, 31.06170, 33.36890,  &
      &     34.67260, 35.31630, 35.56310, 35.92990, 35.76300, 35.65970,  &
      &     35.56450, 35.88470, 36.02280, 36.18740, 36.52540, 36.67060,  &
-     &     36.64880, 36.78810, 36.91850/
+     &     36.64880, 36.78810, 36.91850, 0.0000/
 !
-      DATA B1/1.0000, 20.65930, 9.10370, 3.95460, 43.64270, 23.21850,   &
+      DATA B1/20.65930, 9.10370, 3.95460, 43.64270, 23.21850,   &
      &     20.84390, 0.00570, 13.27710, 10.28250, 8.40420, 3.28500,     &
      &     2.82750, 3.03870, 2.43860, 1.90670, 1.46790, 0.01040,        &
      &     0.90720, 12.79490, 10.44210, 9.02130, 7.85080, 6.86570,      &
@@ -132,9 +131,9 @@
      &     1.51293, 0.46110, 0.54500, 0.65515, 0.69020, 0.70400,        &
      &     0.70100, 0.68587, 0.66310, 0.64645, 0.61634, 0.58909,        &
      &     0.56336, 0.54775, 0.52930, 0.51193, 0.49938, 0.48363,        &
-     &     0.46515, 0.45102, 0.43753/
+     &     0.46515, 0.45102, 0.43753, 1.0000/
 !
-      DATA A2/0.0000, 0.26200, 0.63090, 0.75080, 1.12780, 1.33260,      &
+      DATA A2/0.26200, 0.63090, 0.75080, 1.12780, 1.33260,      &
      &     1.02000, 3.13220, 2.28680, 2.64120, 3.11250, 3.17360,        &
      &     2.17350, 1.90020, 3.03530, 4.17910, 5.20340, 7.19640,        &
      &     6.77230, 7.43980, 7.38730, 7.36790, 7.35580, 7.35110,        &
@@ -150,9 +149,9 @@
      &     17.76390, 18.59130, 19.04170, 19.15840, 13.06370, 12.95100,  &
      &     15.47330, 19.02110, 21.28160, 23.05470, 22.90640, 23.10320,  &
      &     23.42190, 23.29480, 23.41280, 23.59640, 23.80830, 24.09920,  &
-     &     24.40960, 24.77360, 25.19950/
+     &     24.40960, 24.77360, 25.19950, 0.0000/
 !
-      DATA B2/1.0000, 7.74039, 3.35680, 1.05240, 1.86230, 1.02100,      &
+      DATA B2/7.74039, 3.35680, 1.05240, 1.86230, 1.02100,      &
      &     10.20750, 9.89330, 5.70110, 4.29440, 3.42620, 8.84220,       &
      &     79.26110, 0.74260, 32.33370, 27.15700, 22.21510, 1.16620,    &
      &     14.84070, 0.77480, 0.65990, 0.57290, 0.50000, 0.43850,       &
@@ -168,9 +167,9 @@
      &     8.81174, 8.62160, 8.44840, 8.70751, 2.35760, 2.92380,        &
      &     3.55078, 3.97458, 4.06910, 4.17619, 3.87135, 3.65155,        &
      &     3.46204, 3.41519, 3.32530, 3.25396, 3.26371, 3.20647,        &
-     &     3.08997, 3.04619, 3.00775/
+     &     3.08997, 3.04619, 3.00775, 1.0000/
 !
-      DATA A3/0.0000, 0.19677, 0.31120, 0.61750, 0.53910, 1.09790,      &
+      DATA A3/0.19677, 0.31120, 0.61750, 0.53910, 1.09790,      &
      &     1.58860, 2.01250, 1.54630, 1.51700, 1.45460, 1.26740,        &
      &     1.22690, 1.59360, 1.98910, 1.78000, 1.43790, 6.25560,        &
      &     0.65390, 1.05190, 1.58990, 1.64090, 1.69910, 2.07030,        &
@@ -186,9 +185,9 @@
      &     15.71310, 25.55820, 21.65750, 15.53800, 18.44200, 16.58770,  &
      &     13.11380, 9.49887, 8.00370, 12.14390, 12.47390, 12.59770,    &
      &     12.74730, 14.18910, 14.94910, 15.64020, 16.77070, 17.34150,  &
-     &     17.39900, 17.89190, 18.33170/
+     &     17.39900, 17.89190, 18.33170, 0.0000/
 !
-      DATA B3/1.0000, 49.55190, 22.92760, 85.39050, 103.4830, 60.34980, &
+      DATA B3/49.55190, 22.92760, 85.39050, 103.4830, 60.34980, &
      &     0.56870, 28.99750, 0.32390, 0.26150, 0.23060, 0.31360,       &
      &     0.38080, 31.54720, 0.67850, 0.52600, 0.25360, 18.51940,      &
      &     43.89830, 213.18700, 85.74840, 136.10800, 35.63380, 26.89380,&
@@ -204,9 +203,9 @@
      &     0.42459, 1.48260, 1.57290, 1.96347, 8.61800, 8.79370,        &
      &     9.55642, 11.38240, 14.04220, 23.10520, 19.98870, 18.59900,   &
      &     17.83090, 16.92350, 16.09270, 15.36220, 14.94550, 14.31360,  &
-     &     13.43460, 12.89460, 12.40440/
+     &     13.43460, 12.89460, 12.40440, 1.0000/
 !
-      DATA A4/0.0000, 0.04988, 0.17800, 0.46530, 0.70290, 0.10680,      &
+      DATA A4/0.04988, 0.17800, 0.46530, 0.70290, 0.10680,      &
      &     0.86500, 1.16630, 0.86700, 1.02430, 1.12510, 1.11280,        &
      &     2.30730, 1.96460, 1.54100, 1.49080, 1.58630, 1.64550,        &
      &     1.64420, 0.86590, 1.02110, 1.46800, 1.90210, 2.05710,        &
@@ -222,9 +221,9 @@
      &     5.78370, 5.86000, 5.96760, 5.52593, 5.96960, 6.46920,        &
      &     7.02588, 7.42518, 7.44330, 2.11253, 3.21097, 4.08655,        &
      &     4.80703, 4.17287, 4.18800, 4.18550, 3.47947, 3.49331,        &
-     &     4.21665, 4.23284, 4.24391/
+     &     4.21665, 4.23284, 4.24391, 0.0000/
 !
-      DATA B4/1.0000, 2.20159, 0.98210, 168.26100, 0.54200, 0.14030,    &
+      DATA B4/2.20159, 0.98210, 168.26100, 0.54200, 0.14030,    &
      &     51.65120, 0.58260, 32.90890, 26.14760, 21.71840, 129.42400,  &
      &     7.19370, 85.08860, 81.69370, 68.16450, 56.17200, 47.77840,   &
      &     33.39290, 41.68410, 178.43700, 51.35310, 116.10500,          &
@@ -241,9 +240,9 @@
      &     38.61030, 36.39560, 38.32460, 45.81490, 47.25790, 48.00930,  &
      &     47.00450, 45.47150, 44.24730, 150.64500, 142.32500,          &
      &     117.02000, 99.17220, 105.25100, 100.61300, 97.49080,         &
-     &     105.98000, 102.27300, 88.48340, 86.00300, 83.78810/
+     &     105.98000, 102.27300, 88.48340, 86.00300, 83.78810, 1.0000/
 !
-      DATA CV/0.0000, 0.00131, 0.00640, 0.03770, 0.03850, -0.19320,     &
+      DATA CV/0.00131, 0.00640, 0.03770, 0.03850, -0.19320,     &
      &     0.21560, -11.52900, 0.25080, 0.27760, 0.35150, 0.67600,      &
      &     0.85840, 1.11510, 1.14070, 1.11490, 0.86690, -9.55740,       &
      &     1.44450, 1.42280, 1.37510, 1.33290, 1.28070, 1.21990,        &
@@ -259,21 +258,130 @@
      &     11.68830, 12.06580, 12.60890, 13.17460, 13.41180, 13.57820,  &
      &     13.67700, 13.71080, 13.69050, 13.72470, 13.62110, 13.52660,  &
      &     13.43140, 13.42870, 13.39660, 13.35730, 13.38120, 13.35920,  &
-     &     13.28870, 13.27540, 13.26740/
+     &     13.28870, 13.27540, 13.26740, 0.0000/
 !
       DO I = 1, melem
         IF (asym.EQ.symba(I)) THEN
           ascfac = a1(I)*EXP(-b1(I)*ss) + a2(I)*EXP(-b2(I)*ss) + a3(I)  &
      &             *EXP(-b3(I)*ss) + a4(I)*EXP(-b4(I)*ss) + cv(I)
-          GOTO 999
+          RETURN
         ENDIF
       ENDDO
 ! default is a dummy.
       ascfac = a1(1)*EXP(-b1(1)*ss) + a2(1)*EXP(-b2(1)*ss) + a3(1)      &
      &         *EXP(-b3(1)*ss) + a4(1)*EXP(-b4(1)*ss) + cv(1)
-  999 RETURN
 !
       END FUNCTION ASCFAC
+!
+!*****************************************************************************
+!
+      SUBROUTINE Create_AtomicWeightings
+! This routine sets the weights for the atoms used when calculating the centre of mass
+! of a z-matrix.
+! The weights are set such that the 'crystallographic' centre of mass,
+! i.e. the centre of scattering power, is used for rotations.
+! The scattering power of an atom is the square of the number of its electrons,
+! which is taken to be Z, its atomic number, i.e. we are neglecting ions.
+! Just for mathematical rigor, the atomic weights of a z-matrix for which the origin
+! of rotation has been fixed to coincide with an atom (because it is on a special position)
+! are all set to 0.0, except for the atom at the origin which is assigned a weight 1.0
+! Ideally, we would want hydrogens to have weight 0.0 when not taken into account.
+
+      IMPLICIT NONE
+
+      INCLUDE 'PARAMS.INC'
+      INCLUDE 'IZMCheck.inc'
+
+      INTEGER         nfrag
+      COMMON /frgcom/ nfrag
+
+      INTEGER         icomflg
+      REAL                             AtomicWeighting
+      COMMON /zmcomg/ icomflg(maxfrg), AtomicWeighting(maxatm,maxfrg)
+
+      INTEGER         ntatm, natoms
+      INTEGER         ioptb,                iopta,                ioptt
+      INTEGER         iz1,                  iz2,                  iz3
+      COMMON /zmcomi/ ntatm, natoms(maxfrg),                                             &
+     &                ioptb(maxatm,maxfrg), iopta(maxatm,maxfrg), ioptt(maxatm,maxfrg),  &
+     &                iz1(maxatm,maxfrg),   iz2(maxatm,maxfrg),   iz3(maxatm,maxfrg)
+
+      CHARACTER*3     asym
+      CHARACTER*5                          OriginalLabel
+      COMMON /zmcomc/ asym(maxatm,maxfrg), OriginalLabel(maxatm,maxfrg)
+
+      CHARACTER*3 el(1:109)
+      INTEGER     atnr(1:109)
+! Elements (plus other CSD 'element' definitions What's 'Zz'??)
+! Me = methyl, Du = Dummy
+      DATA el  /'C  ', 'H  ', 'Ac ', 'Ag ', 'Al ', 'Am ', 'Ar ', 'As ', 'At ', 'Au ',   &
+                'B  ', 'Ba ', 'Be ', 'Bi ', 'Bk ', 'Br ', 'Ca ', 'Cd ', 'Ce ', 'Cf ',   &
+                'Cl ', 'Cm ', 'Co ', 'Cr ', 'Cs ', 'Cu ', 'D  ', 'Dy ', 'Er ', 'Es ',   &
+                'Eu ', 'F  ', 'Fe ', 'Fm ', 'Fr ', 'Ga ', 'Gd ', 'Ge ', 'He ', 'Hf ',   &
+                'Hg ', 'Ho ', 'I  ', 'In ', 'Ir ', 'K  ', 'Kr ', 'La ', 'Li ', 'Lu ',   &
+                'Lw ', 'Md ', 'Mg ', 'Mn ', 'Mo ', 'N  ', 'Na ', 'Nb ', 'Nd ', 'Ne ',   &
+                'Ni ', 'No ', 'Np ', 'O  ', 'Os ', 'P  ', 'Pa ', 'Pb ', 'Pd ', 'Pm ',   &
+                'Po ', 'Pr ', 'Pt ', 'Pu ', 'Ra ', 'Rb ', 'Re ', 'Rh ', 'Rn ', 'Ru ',   &
+                'S  ', 'Sb ', 'Sc ', 'Se ', 'Si ', 'Sm ', 'Sn ', 'Sr ', 'Ta ', 'Tb ',   &
+                'Tc ', 'Te ', 'Th ', 'Ti ', 'Tl ', 'Tm ', 'U  ', 'V  ', 'W  ', 'X  ',   &
+                'Xe ', 'Y  ', 'Yb ', 'Z  ', 'Zn ', 'Zr ', 'Zz ', 'Me ', 'Du '/
+
+! Elements (plus other CSD 'element' definitions What's 'Zz'??)
+      DATA atnr/    6,     1,    89,    47,    13,    95,    18,    33,    85,    79,   &
+                    5,    56,     4,    83,    97,    35,    20,    48,    58,    98,   &
+                   17,    96,    27,    24,    55,    29,     0,    66,    68,    99,   &
+                   63,     9,    26,   100,    87,    31,    64,    32,     2,    72,   &
+                   80,    67,    53,    49,    77,    19,    36,    57,     3,    71,   &
+                    0,   101,    12,    25,    42,     7,    11,    41,    60,    10,   &
+                   28,   102,    93,     8,    76,    15,    91,    82,    46,    61,   &
+                   84,    59,    78,    94,    88,    37,    75,    45,    86,    44,   &
+                   16,    51,    21,    34,    14,    62,    50,    38,    73,    65,   &
+                   43,    52,    90,    22,    81,    69,    92,    23,    74,     0,   &
+                   54,    39,    70,     0,    30,    40,     0,     0,     0/
+
+      INTEGER IFrag, jj, I, J
+      REAL    TotalAtomicWeighting
+      LOGICAL UseCrystallographicCentreOfMass
+
+      UseCrystallographicCentreOfMass = .TRUE.
+      IFrag = 0
+      DO jj = 1, nfrag
+        DO WHILE (IFrag.LE.CheckSize)
+          IFrag = IFrag + 1
+          IF (IZMCheck(IFrag).EQ.1) EXIT     ! the loop since we have a fragment we are using
+        ENDDO
+        IF (icomflg(IFrag) .EQ. 0)  THEN
+          IF (UseCrystallographicCentreOfMass) THEN
+            DO I = 1, natoms(IFrag)
+              DO J = 1, 109
+                IF (asym(I,IFrag) .EQ. el(J)) THEN
+                  AtomicWeighting(I,IFrag) = FLOAT(atnr(J))**2
+                  EXIT
+                ENDIF
+              ENDDO
+            ENDDO
+          ELSE
+            TotalAtomicWeighting = 0.0
+            DO I = 1, natoms(IFrag)
+              AtomicWeighting(I,IFrag) = 1.0
+            ENDDO
+          ENDIF
+        ELSE
+          DO I = 1, natoms(IFrag)
+            AtomicWeighting(I,IFrag) = 0.0
+          ENDDO
+          AtomicWeighting(icomflg(IFrag),IFrag) = 1.0
+        ENDIF
+        TotalAtomicWeighting = 0.0
+        DO I = 1, natoms(IFrag)
+          TotalAtomicWeighting = TotalAtomicWeighting + AtomicWeighting(I,IFrag) 
+        ENDDO
+        DO I = 1, natoms(IFrag)
+          AtomicWeighting(I,IFrag) = AtomicWeighting(I,IFrag) / TotalAtomicWeighting 
+        ENDDO
+      ENDDO
+
+      END SUBROUTINE Create_AtomicWeightings
 !
 !*****************************************************************************
 !
