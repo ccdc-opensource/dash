673b639cda13864f3ccb0a14e417715f43904fac
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2001-10-26 09:17:40 +0000

    Removed ocd that addressed non-existing fields Number of moves progress bar now correct if not a multirun

--- SA_output.f90
+++ SA_output.f90
@@ -8,10 +8,16 @@
       USE WINTERACTER
       USE DRUID_HEADER
 
+      IMPLICIT NONE
+
+      INTEGER kopt
+      REAL T, CHIMIN, CHIAV, CHIESD
+      INTEGER n, NUP, NDOWN, NREJ, nmpert, ntotmov, iteration
       REAL*8 xopt(*),dxvav(*),xvsig(*),flav(*),lb(*),ub(*),vm(*)
 
       INCLUDE 'PARAMS.INC'
 
+      REAL             CHIPROBEST
       COMMON /PLTSTO2/ CHIPROBEST(MAXITER)
       REAL             PAWLEYCHISQ, RWPOBS, RWPEXP
       COMMON /PRCHISQ/ PAWLEYCHISQ, RWPOBS, RWPEXP
@@ -23,38 +29,34 @@
                 tempvl, avchi1, avchi2, avchi3, avchi4, nd1, &
                 nd2, nd3, nd4, bmIHANDLE
 
+      REAL ctem
+
       CALL WDialogSelect(IDD_SA_Action1)
       CALL WDialogPutReal(IDF_curr_temp,T,'(F8.2)')
       CALL WDialogPutReal(IDF_min_chisq,chimin,'(F8.2)')
       CALL WDialogPutReal(IDF_profile_chisq2,CHIPROBEST(iteration),'(F8.2)')
-
-!.. best chi-squared scale ...
-      bchmin=alog10(max(1.0,chimin))
-!.. profile chi-squared scale ...
-      bpwval = alog10(MAX(1.0,PAWLEYCHISQ))
-      bchpro = alog10(MAX(1.0,CHIPROBEST(iteration)))
+! best chi-squared scale
+      bchmin = ALOG10(MAX(1.0,chimin))
+! profile chi-squared scale
+      bpwval = ALOG10(MAX(1.0,PAWLEYCHISQ))
+      bchpro = ALOG10(MAX(1.0,CHIPROBEST(iteration)))
+! What is 'kopt' supposed to do?
       IF (kopt .EQ. 1) THEN
         CALL WDialogPutReal(IDF_av_chisq,chiav,'(F8.2)')
-        CALL WDialogPutReal(IDF_rms_chisq,chiesd,'(F8.2)')
         CALL WDialogPutInteger(IDF_total_moves,ntotmov)
-        CALL WDialogPutInteger(IDF_moves_per_T,nmpert)
-        CALL WDialogPutInteger(IDF_downhill_moves,ndown)
-        CALL WDialogPutInteger(IDF_rej_up_moves,nrej)
-        CALL WDialogPutInteger(IDF_acc_up_moves,nup)
         CALL WDialogPutInteger(IDF_SA_total_moves_label,nmpert)
-!.. Temperature scale ...
-        tempvl = alog10(MAX(1.0,t))
-        avchi1 = alog10(MAX(1.0,chiav-chiesd))
+! Temperature scale
+        tempvl = ALOG10(MAX(1.0,t))
+        avchi1 = ALOG10(MAX(1.0,chiav-chiesd))
         ctem = MAX(1.0,chiav+chiesd)
-        avchi2 = alog10(MIN(10000.0,ctem))
-        avchi3 = alog10(MAX(1.0,chiav-0.1*chiesd))
+        avchi2 = ALOG10(MIN(10000.0,ctem))
+        avchi3 = ALOG10(MAX(1.0,chiav-0.1*chiesd))
         ctem = MAX(1.0,chiav+0.1*chiesd)
-        avchi4 = alog10(MIN(10000.0,ctem))
+        avchi4 = ALOG10(MIN(10000.0,ctem))
         nd1 = ndown
-        nd2 = nmpert
         nd3 = ntotmov
         nd4 = nup
-      END IF
+      ENDIF
       CALL Sa_Output_Gr
 ! Following lines write values to dialogue that hasn't been loaded into memory,
 ! nor is it displayed.
@@ -85,6 +87,8 @@
 
       IMPLICIT NONE
 
+      INCLUDE 'PARAMS.INC'
+
       REAL bchmin, bpwval, bchpro, tempvl
       REAL avchi1, avchi2, avchi3, avchi4
       INTEGER nd1, nd2, nd3, nd4, bmIHANDLE
@@ -107,16 +111,16 @@
       CALL IGrUnits(0.0,0.0,4.0,1.0)
       CALL WBitmapPut(bmIHANDLE,0,0)
       CALL IGrColourN(95) ! Lightgreen
-      tenow1=tempvl-0.03
-      tenow2=tempvl+0.03
+      tenow1 = tempvl-0.03
+      tenow2 = tempvl+0.03
       CALL IGrRectangle(tenow1,rminh,tenow2,rmaxh)
 ! Minimum chi squared
       CALL IGrSelect(3,IDF_minchisq_picture)
       CALL IGrUnits(0.0,0.0,4.0,1.0)
       CALL WBitmapPut(bmIHANDLE,0,0)
       CALL IGrColourN(95) ! Lightgreen
-      tenow1=bchmin-0.03
-      tenow2=bchmin+0.03
+      tenow1 = bchmin-0.03
+      tenow2 = bchmin+0.03
       CALL IGrRectangle(tenow1,rminh,tenow2,rmaxh)
 ! Average chi-squared
       CALL IGrSelect(3,IDF_avchisq_picture)
@@ -131,45 +135,48 @@
       CALL IGrUnits(0.0,0.0,4.0,1.0)
       CALL WBitmapPut(bmIHANDLE,0,0)
       CALL IGrColourN(63)
-      tenow1=bpwval-0.03
-      tenow2=bpwval+0.03
+      tenow1 = bpwval-0.03
+      tenow2 = bpwval+0.03
       CALL IGrRectangle(tenow1,rminh,tenow2,rmaxh)
       CALL IGrColourN(95) ! Lightgreen
-      tenow1=bchpro-0.03
-      tenow2=bchpro+0.03
+      tenow1 = bchpro-0.03
+      tenow2 = bchpro+0.03
       CALL IGrRectangle(tenow1,rminh,tenow2,rmaxh)
 ! Total moves
       CALL IGrSelect(3,IDF_SATotalMoves_picture)
-      ruler = MaxMoves
+      IF (RESTART) THEN
+        ruler = FLOAT(MaxMoves)
+      ELSE
+        ruler = MaxIter * nd2
+      ENDIF
       CALL IGrUnits(0.0,0.0,ruler,1.0)
       CALL IGrColourN(59)  ! Yellow
-      rulex1=0.0
-      rulex2=nd3
+      rulex1 = 0.0
+      rulex2 = FLOAT(nd3)
       CALL IGrRectangle(rulex1,rminh,rulex2,rmaxh)
       CALL IGrColourN(159) ! Blue
-      rulex1=rulex2
-      rulex2=ruler
+      rulex1 = rulex2
+      rulex2 = ruler
       CALL IGrRectangle(rulex1,rminh,rulex2,rmaxh)
 ! Uphill, downhill, rejected
       CALL IGrSelect(3,IDF_SA_move_distribution)
-      ruler=nd2
+      ruler = FLOAT(nd2)
       CALL IGrUnits(0.0,0.0,ruler,1.0)
       CALL IGrColourN(95) ! Lightgreen
-      rulex1=0.0
-      rulex2=nd1
+      rulex1 = 0.0
+      rulex2 = FLOAT(nd1)
       CALL IGrRectangle(rulex1,rminh,rulex2,rmaxh)
       CALL IGrColourN(159) ! Blue
-      rulex1=rulex2
-      rulex2=rulex2+nd4
+      rulex1 = rulex2
+      rulex2 = rulex2 + FLOAT(nd4)
       CALL IGrRectangle(rulex1,rminh,rulex2,rmaxh)
       CALL IGrColourN(31) ! Red
-      rulex1=rulex2
-      rulex2=nd2
+      rulex1 = rulex2
+      rulex2 = ruler
       CALL IGrRectangle(rulex1,rminh,rulex2,rmaxh)
       CALL IGrSelect(1,0)
       CALL IGrUnits(0.0,0.0,1.0,1.0)
       CALL IGrArea(0.0,0.0,1.0,1.0)
-      RETURN
 
       END SUBROUTINE sa_output_gr
 !
