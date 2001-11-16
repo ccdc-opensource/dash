68910596951962774f7b86f68ab8da4d0c65179b
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2001-11-16 17:31:32 +0000

    Commented out unused code

--- Eval.f90
+++ Eval.f90
@@ -1,146 +1,146 @@
-!*==MAKEFRAC.f90  processed by SPAG 6.11Dc at 13:14 on 17 Sep 2001
-!
-      SUBROUTINE MAKEFRAC(CHROM)
-
-      USE ZMVAR
-
-      PARAMETER (mvar=100)
-      REAL*8 CHROM(*)
-      REAL*8 CKK1, CKK2, CKK3
-      REAL*8 TRAN(3), ROTA(3,3), POS(3,MAXATM), CART(MAXATM,3)
-      REAL*8 QUATER(4), QQSUM, QDEN, QUATT(MVAR)
-      REAL*8 XC, YC, ZC, ZERO, ONE, V1, V2, V3
-
-      INTEGER         NATOM
-      REAL                   X
-      INTEGER                          KX
-      REAL                                        AMULT,      TF
-      INTEGER         KTF
-      REAL                      SITE
-      INTEGER                              KSITE,      ISGEN
-      REAL            SDX,        SDTF,      SDSITE
-      INTEGER                                             KOM17
-      COMMON /POSNS / NATOM, X(3,150), KX(3,150), AMULT(150), TF(150),  &
-     &                KTF(150), SITE(150), KSITE(150), ISGEN(3,150),    &
-     &                SDX(3,150), SDTF(150), SDSITE(150), KOM17
-      DATA ZERO, ONE/0.0D0, 1.0D0/
-      LOGICAL UseCrystallographicCentreOfMass
-
-      KK = 0
-      KATOM = 0
-!.. Loop over all the fragments
-      DO ifrg = 1, maxfrg
-        IF (gotzmfile(ifrg)) THEN
-          NATS = NATOMS(ifrg)
-          KK1 = KK + 1     ! x-translation
-          KK2 = KK + 2     ! y-translation
-          KK3 = KK + 3     ! z-translation
-          CHROM(KK1) = CHROM(KK1) - INT(CHROM(KK1))  ! Position centre of mass inside unit cell
-          CHROM(KK2) = CHROM(KK2) - INT(CHROM(KK2))
-          CHROM(KK3) = CHROM(KK3) - INT(CHROM(KK3))
-          CKK1 = CHROM(KK1)
-          CKK2 = CHROM(KK2)
-          CKK3 = CHROM(KK3)
-          TRAN(1) = CKK1*F2CMAT(1,1)
-          TRAN(2) = CKK1*F2CMAT(1,2) + CKK2*F2CMAT(2,2)
-          TRAN(3) = CKK1*F2CMAT(1,3) + CKK2*F2CMAT(2,3) + CKK3*F2CMAT(3,3)
-          KK = KK + 3
-! If more than one atom then proceed
-          IF (NATS.GT.1) THEN
-            QQSUM = 0.
-            DO JQ = 1, 4
-              JQS = JQ + KK
-              QUATT(JQS) = CHROM(JQS)
-              QQSUM = QQSUM + QUATT(JQS)**2
-            ENDDO
-! QQSUM now holds the sum of the squares of the quaternions
-            QDEN = 1./SQRT(QQSUM)
-            DO JQ = 1, 4
-              JQS = JQ + KK
-              QUATT(JQS) = QDEN*QUATT(JQS)
-              QUATER(JQ) = QUATT(JQS)
-            ENDDO
-! QUATER now holds the normalised quaternions
-            CALL ROTMAK(QUATER,ROTA)
-! ROTA now holds the 3x3 rotation matrix corresponding to the quaternions
-            KK = KK + 4
-          ENDIF
-          DO I = 1, NATS
-            IF (IOPTB(I,IFRG).EQ.1) THEN
-              KK = KK + 1
-              BLEN(I,IFRG) = CHROM(KK)
-            ENDIF
-            IF (IOPTA(I,IFRG).EQ.1) THEN
-              KK = KK + 1
-              ALPH(I,IFRG) = CHROM(KK)
-            ENDIF
-            IF (IOPTT(I,IFRG).EQ.1) THEN
-              KK = KK + 1
-              BET(I,IFRG) = CHROM(KK)
-            ENDIF
-          ENDDO
-          CALL MAKEXYZ(NATS,BLEN(1,ifrg),ALPH(1,ifrg),BET(1,ifrg),        &
-       &               IZ1(1,ifrg),IZ2(1,ifrg),IZ3(1,ifrg),CART(1,1),     &
-       &               CART(1,2),CART(1,3))
-! Determine origin for rotations
-          ICFRG = ICOMFLG(ifrg)
-! If user set centre of mass flag to 0, then use the molecule's centre of mass
-          IF (ICFRG.EQ.0) THEN
-            XC = ZERO
-            YC = ZERO
-            ZC = ZERO
-            UseCrystallographicCentreOfMass = .TRUE.
-            IF (UseCrystallographicCentreOfMass) THEN
-              DO I = 1, NATS
-                XC = XC + AtomicWeighting(I,ifrg)*CART(I,1)
-                YC = YC + AtomicWeighting(I,ifrg)*CART(I,2)
-                ZC = ZC + AtomicWeighting(I,ifrg)*CART(I,3)
-              ENDDO
-            ELSE
-              DO I = 1, NATS
-                XC = XC + CART(I,1)
-                YC = YC + CART(I,2)
-                ZC = ZC + CART(I,3)
-              ENDDO
-              XC = XC/DBLE(NATS)
-              YC = YC/DBLE(NATS)
-              ZC = ZC/DBLE(NATS)
-            ENDIF
-! Otherwise, use atom number ICFRG
-          ELSE
-            XC = CART(ICFRG,1)
-            YC = CART(ICFRG,2)
-            ZC = CART(ICFRG,3)
-          ENDIF
-! Subtract the origin from all atom positions
-          DO I = 1, NATS
-            CART(I,1) = CART(I,1) - XC
-            CART(I,2) = CART(I,2) - YC
-            CART(I,3) = CART(I,3) - ZC
-          ENDDO
-          DO I = 1, NATS
-            POS(1,I) = CART(I,1)
-            POS(2,I) = CART(I,2)
-            POS(3,I) = CART(I,3)   ! Last occurrence of CART in this file: we're switching to POS
-          ENDDO
-          CALL DO_ATOM_POS(TRAN,ROTA,POS,NATS)
-          V1 = ONE/F2CMAT(1,1)
-          V2 = ONE/F2CMAT(2,2)
-          V3 = ONE/F2CMAT(3,3)
-          DO I = 1, NATS
-            POS(1,I) = POS(1,I)*V1
-            POS(2,I) = (POS(2,I)-POS(1,I)*F2CMAT(1,2))*V2
-            POS(3,I) = (POS(3,I)-POS(1,I)*F2CMAT(1,3)-POS(2,I)*F2CMAT(2,3))*V3
-            KI = KATOM + I
-            X(1,KI) = SNGL(POS(1,I))
-            X(2,KI) = SNGL(POS(2,I))
-            X(3,KI) = SNGL(POS(3,I))
-          ENDDO
-          KATOM = KATOM + NATS
-        ENDIF
-      ENDDO
-
-      END SUBROUTINE MAKEFRAC
+!O!*==MAKEFRAC.f90  processed by SPAG 6.11Dc at 13:14 on 17 Sep 2001
+!O!
+!O      SUBROUTINE MAKEFRAC(CHROM)
+!O
+!O      USE ZMVAR
+!O
+!O      PARAMETER (mvar=100)
+!O      REAL*8 CHROM(*)
+!O      REAL*8 CKK1, CKK2, CKK3
+!O      REAL*8 TRAN(3), ROTA(3,3), POS(3,MAXATM), CART(MAXATM,3)
+!O      REAL*8 QUATER(4), QQSUM, QDEN, QUATT(MVAR)
+!O      REAL*8 XC, YC, ZC, ZERO, ONE, V1, V2, V3
+!O
+!O      INTEGER         NATOM
+!O      REAL                   X
+!O      INTEGER                          KX
+!O      REAL                                        AMULT,      TF
+!O      INTEGER         KTF
+!O      REAL                      SITE
+!O      INTEGER                              KSITE,      ISGEN
+!O      REAL            SDX,        SDTF,      SDSITE
+!O      INTEGER                                             KOM17
+!O      COMMON /POSNS / NATOM, X(3,150), KX(3,150), AMULT(150), TF(150),  &
+!O     &                KTF(150), SITE(150), KSITE(150), ISGEN(3,150),    &
+!O     &                SDX(3,150), SDTF(150), SDSITE(150), KOM17
+!O      DATA ZERO, ONE/0.0D0, 1.0D0/
+!O      LOGICAL UseCrystallographicCentreOfMass
+!O
+!O      KK = 0
+!O      KATOM = 0
+!O!.. Loop over all the fragments
+!O      DO ifrg = 1, maxfrg
+!O        IF (gotzmfile(ifrg)) THEN
+!O          NATS = NATOMS(ifrg)
+!O          KK1 = KK + 1     ! x-translation
+!O          KK2 = KK + 2     ! y-translation
+!O          KK3 = KK + 3     ! z-translation
+!O          CHROM(KK1) = CHROM(KK1) - INT(CHROM(KK1))  ! Position centre of mass inside unit cell
+!O          CHROM(KK2) = CHROM(KK2) - INT(CHROM(KK2))
+!O          CHROM(KK3) = CHROM(KK3) - INT(CHROM(KK3))
+!O          CKK1 = CHROM(KK1)
+!O          CKK2 = CHROM(KK2)
+!O          CKK3 = CHROM(KK3)
+!O          TRAN(1) = CKK1*F2CMAT(1,1)
+!O          TRAN(2) = CKK1*F2CMAT(1,2) + CKK2*F2CMAT(2,2)
+!O          TRAN(3) = CKK1*F2CMAT(1,3) + CKK2*F2CMAT(2,3) + CKK3*F2CMAT(3,3)
+!O          KK = KK + 3
+!O! If more than one atom then proceed
+!O          IF (NATS.GT.1) THEN
+!O            QQSUM = 0.
+!O            DO JQ = 1, 4
+!O              JQS = JQ + KK
+!O              QUATT(JQS) = CHROM(JQS)
+!O              QQSUM = QQSUM + QUATT(JQS)**2
+!O            ENDDO
+!O! QQSUM now holds the sum of the squares of the quaternions
+!O            QDEN = 1./SQRT(QQSUM)
+!O            DO JQ = 1, 4
+!O              JQS = JQ + KK
+!O              QUATT(JQS) = QDEN*QUATT(JQS)
+!O              QUATER(JQ) = QUATT(JQS)
+!O            ENDDO
+!O! QUATER now holds the normalised quaternions
+!O            CALL ROTMAK(QUATER,ROTA)
+!O! ROTA now holds the 3x3 rotation matrix corresponding to the quaternions
+!O            KK = KK + 4
+!O          ENDIF
+!O          DO I = 1, NATS
+!O            IF (IOPTB(I,IFRG).EQ.1) THEN
+!O              KK = KK + 1
+!O              BLEN(I,IFRG) = CHROM(KK)
+!O            ENDIF
+!O            IF (IOPTA(I,IFRG).EQ.1) THEN
+!O              KK = KK + 1
+!O              ALPH(I,IFRG) = CHROM(KK)
+!O            ENDIF
+!O            IF (IOPTT(I,IFRG).EQ.1) THEN
+!O              KK = KK + 1
+!O              BET(I,IFRG) = CHROM(KK)
+!O            ENDIF
+!O          ENDDO
+!O          CALL MAKEXYZ(NATS,BLEN(1,ifrg),ALPH(1,ifrg),BET(1,ifrg),        &
+!O       &               IZ1(1,ifrg),IZ2(1,ifrg),IZ3(1,ifrg),CART(1,1),     &
+!O       &               CART(1,2),CART(1,3))
+!O! Determine origin for rotations
+!O          ICFRG = ICOMFLG(ifrg)
+!O! If user set centre of mass flag to 0, then use the molecule's centre of mass
+!O          IF (ICFRG.EQ.0) THEN
+!O            XC = ZERO
+!O            YC = ZERO
+!O            ZC = ZERO
+!O            UseCrystallographicCentreOfMass = .TRUE.
+!O            IF (UseCrystallographicCentreOfMass) THEN
+!O              DO I = 1, NATS
+!O                XC = XC + AtomicWeighting(I,ifrg)*CART(I,1)
+!O                YC = YC + AtomicWeighting(I,ifrg)*CART(I,2)
+!O                ZC = ZC + AtomicWeighting(I,ifrg)*CART(I,3)
+!O              ENDDO
+!O            ELSE
+!O              DO I = 1, NATS
+!O                XC = XC + CART(I,1)
+!O                YC = YC + CART(I,2)
+!O                ZC = ZC + CART(I,3)
+!O              ENDDO
+!O              XC = XC/DBLE(NATS)
+!O              YC = YC/DBLE(NATS)
+!O              ZC = ZC/DBLE(NATS)
+!O            ENDIF
+!O! Otherwise, use atom number ICFRG
+!O          ELSE
+!O            XC = CART(ICFRG,1)
+!O            YC = CART(ICFRG,2)
+!O            ZC = CART(ICFRG,3)
+!O          ENDIF
+!O! Subtract the origin from all atom positions
+!O          DO I = 1, NATS
+!O            CART(I,1) = CART(I,1) - XC
+!O            CART(I,2) = CART(I,2) - YC
+!O            CART(I,3) = CART(I,3) - ZC
+!O          ENDDO
+!O          DO I = 1, NATS
+!O            POS(1,I) = CART(I,1)
+!O            POS(2,I) = CART(I,2)
+!O            POS(3,I) = CART(I,3)   ! Last occurrence of CART in this file: we're switching to POS
+!O          ENDDO
+!O          CALL DO_ATOM_POS(TRAN,ROTA,POS,NATS)
+!O          V1 = ONE/F2CMAT(1,1)
+!O          V2 = ONE/F2CMAT(2,2)
+!O          V3 = ONE/F2CMAT(3,3)
+!O          DO I = 1, NATS
+!O            POS(1,I) = POS(1,I)*V1
+!O            POS(2,I) = (POS(2,I)-POS(1,I)*F2CMAT(1,2))*V2
+!O            POS(3,I) = (POS(3,I)-POS(1,I)*F2CMAT(1,3)-POS(2,I)*F2CMAT(2,3))*V3
+!O            KI = KATOM + I
+!O            X(1,KI) = SNGL(POS(1,I))
+!O            X(2,KI) = SNGL(POS(2,I))
+!O            X(3,KI) = SNGL(POS(3,I))
+!O          ENDDO
+!O          KATOM = KATOM + NATS
+!O        ENDIF
+!O      ENDDO
+!O
+!O      END SUBROUTINE MAKEFRAC
 !*==MAKEFRAC.f90  processed by SPAG 6.11Dc at 13:14 on 17 Sep 2001
 !
       SUBROUTINE MAKEFRAC_2(CHROM)
--- Vicar.f90
+++ Vicar.f90
@@ -1,149 +1,149 @@
-!*==MAKEXYZ.f90  processed by SPAG 6.11Dc at 13:14 on 17 Sep 2001
-      SUBROUTINE makexyz(n,blen,alph,bet,iz1,iz2,iz3,x,y,z)
-
-      IMPLICIT NONE
-
-! Arguments
-      INTEGER n
-      INTEGER iz1(*), iz2(*), iz3(*)
-      DOUBLE PRECISION x(*), y(*), z(*), blen(*), alph(*), bet(*)
-
-! Constants
-      REAL*8 radian, pi, sqrtpi, twosix, logten
-      PARAMETER (radian=57.29577951308232088D0)
-      PARAMETER (pi=3.141592653589793238D0)
-      PARAMETER (sqrtpi=1.772453850905516027D0)
-      PARAMETER (twosix=1.122462048309372981D0)
-      PARAMETER (logten=2.302585092994045684D0)
-
-! Local
-! replace radian with rad=(1/radian)
-      INTEGER i, k, i1, i2, i3
-      REAL*8 bond, angle1, angle2, sign, rad
-!
-! cosmetic change to demo cvs
-      IF (N.LT.1) RETURN
-      rad = 1.0/radian
-!     First atom is placed at the origin
-      x(1) = 0.0D0
-      y(1) = 0.0D0
-      z(1) = 0.0D0
-! Second atom is placed along the z-axis
-      IF (N.LT.2) GOTO 999
-      x(2) = 0.0D0
-      y(2) = 0.0D0
-      z(2) = blen(2)
-! Third atom is placed in the x,z-plane
-      IF (N.LT.3) GOTO 999
-      x(3) = blen(3)*SIN(alph(3)*rad)
-      y(3) = 0.0D0
-      IF (iz1(3).EQ.1) THEN
-        z(3) = blen(3)*COS(alph(3)*rad)
-      ELSE
-        z(3) = z(2) - blen(3)*COS(alph(3)*rad)
-      ENDIF
-! As long as atoms remain linear with the first
-! two atoms, keep placing them along the z-axis
-      i = 3
-      IF (n.GT.3) THEN
-        DO WHILE (NINT(x(i)*10000).EQ.0)
-          i = i + 1
-          i1 = iz1(i)
-          i2 = iz2(i)
-          IF (z(i1).GT.z(i2)) THEN
-            sign = 1.0D0
-          ELSE
-            sign = -1.0D0
-          ENDIF
-          x(i) = blen(i)*SIN(alph(i)*rad)
-          y(i) = 0.0D0
-          z(i) = z(i1) - sign*blen(i)*COS(alph(i)*rad)
-        ENDDO
-      ENDIF
-! Loop over each atom in turn, finding its coordinates
-      k = i + 1
-      IF (k.LE.n) THEN
-        DO i = k, n
-          i1 = iz1(i)
-          i2 = iz2(i)
-          i3 = iz3(i)
-          bond = blen(i)
-          angle1 = alph(i)
-          angle2 = bet(i)
-          CALL xyzatm(x,y,z,i,i1,bond,i2,angle1,i3,angle2)
-        ENDDO
-      ENDIF
-  999 RETURN
-
-      END SUBROUTINE MAKEXYZ
-!*==XYZATM.f90  processed by SPAG 6.11Dc at 13:14 on 17 Sep 2001
-!
-!     "xyzatm" computes the Cartesian coordinates of a single
-!     atom from its defining internal coordinate values
-!
-      SUBROUTINE xyzatm(x,y,z,i,i1,bond,i2,angle1,i3,angle2)
-
-      IMPLICIT NONE
-!
-! Arguments
-      REAL*8 x(*), y(*), z(*)
-      REAL*8 bond, angle1, angle2
-      INTEGER i, i1, i2, i3
-! Constants
-      REAL*8 radian, pi
-      PARAMETER (radian=57.29577951308232088D0)
-      PARAMETER (pi=3.141592653589793238D0)
-      REAL*8 small
-      PARAMETER (small=1.D-8)
-! Local
-      REAL*8 ang_1, ang_2
-      REAL*8 sin_1, cos_1, sin_2, cos_2
-      REAL*8 cosine, one_over_sine, norm, eps, sinarg
-      REAL*8 u1(3), u2(3), u3(3), u4(3), rad
-!
-! convert the angle values from degrees to radians;
-! then find their sine and cosine values
-!
-      eps = 0.00000001D0
-      rad = 1.0/radian
-      ang_1 = angle1*rad
-      ang_2 = angle2*rad
-      sin_1 = SIN(ang_1)
-      cos_1 = COS(ang_1)
-      sin_2 = SIN(ang_2)
-      cos_2 = COS(ang_2)
-      u1(1) = x(i2) - x(i3)
-      u1(2) = y(i2) - y(i3)
-      u1(3) = z(i2) - z(i3)
-      norm = 1.0/SQRT(u1(1)*u1(1)+u1(2)*u1(2)+u1(3)*u1(3))
-      u1 = u1 * norm
-      u2(1) = x(i1) - x(i2)
-      u2(2) = y(i1) - y(i2)
-      u2(3) = z(i1) - z(i2)
-      norm = 1.0/SQRT(u2(1)*u2(1)+u2(2)*u2(2)+u2(3)*u2(3))
-      u2 = u2 * norm
-      u3(1) = u1(2)*u2(3) - u1(3)*u2(2)
-      u3(2) = u1(3)*u2(1) - u1(1)*u2(3)
-      u3(3) = u1(1)*u2(2) - u1(2)*u2(1)
-! Dot product of u1 and u2. As |u1| = |u2| = 1, this is the cosine of the angle between u1 and u2
-      cosine = u1(1)*u2(1) + u1(2)*u2(2) + u1(3)*u2(3)
-! Now there follows a test if the cosine of an angle > 1.0
-! I have to brush up on my mathematics here: I would never have added this test.
-      IF (ABS(cosine).LT.1.0D0) THEN
-        one_over_sine = 1.0/SQRT(1.0D0-cosine**2)
-      ELSE
-        sinarg = DMAX1(small,cosine**2-1.0D0)
-        one_over_sine = 1.0/SQRT(sinarg)
-      ENDIF
-      u3 = u3 * one_over_sine
-      u4(1) = u3(2)*u2(3) - u3(3)*u2(2)
-      u4(2) = u3(3)*u2(1) - u3(1)*u2(3)
-      u4(3) = u3(1)*u2(2) - u3(2)*u2(1)
-      x(i) = x(i1) + bond*(-u2(1)*cos_1 + u4(1)*sin_1*cos_2 + u3(1)*sin_1*sin_2)
-      y(i) = y(i1) + bond*(-u2(2)*cos_1 + u4(2)*sin_1*cos_2 + u3(2)*sin_1*sin_2)
-      z(i) = z(i1) + bond*(-u2(3)*cos_1 + u4(3)*sin_1*cos_2 + u3(3)*sin_1*sin_2)
-
-      END SUBROUTINE XYZATM
+!O!*==MAKEXYZ.f90  processed by SPAG 6.11Dc at 13:14 on 17 Sep 2001
+!O      SUBROUTINE makexyz(n,blen,alph,bet,iz1,iz2,iz3,x,y,z)
+!O
+!O      IMPLICIT NONE
+!O
+!O! Arguments
+!O      INTEGER n
+!O      INTEGER iz1(*), iz2(*), iz3(*)
+!O      DOUBLE PRECISION x(*), y(*), z(*), blen(*), alph(*), bet(*)
+!O
+!O! Constants
+!O      REAL*8 radian, pi, sqrtpi, twosix, logten
+!O      PARAMETER (radian=57.29577951308232088D0)
+!O      PARAMETER (pi=3.141592653589793238D0)
+!O      PARAMETER (sqrtpi=1.772453850905516027D0)
+!O      PARAMETER (twosix=1.122462048309372981D0)
+!O      PARAMETER (logten=2.302585092994045684D0)
+!O
+!O! Local
+!O! replace radian with rad=(1/radian)
+!O      INTEGER i, k, i1, i2, i3
+!O      REAL*8 bond, angle1, angle2, sign, rad
+!O!
+!O! cosmetic change to demo cvs
+!O      IF (N.LT.1) RETURN
+!O      rad = 1.0/radian
+!O!     First atom is placed at the origin
+!O      x(1) = 0.0D0
+!O      y(1) = 0.0D0
+!O      z(1) = 0.0D0
+!O! Second atom is placed along the z-axis
+!O      IF (N.LT.2) GOTO 999
+!O      x(2) = 0.0D0
+!O      y(2) = 0.0D0
+!O      z(2) = blen(2)
+!O! Third atom is placed in the x,z-plane
+!O      IF (N.LT.3) GOTO 999
+!O      x(3) = blen(3)*SIN(alph(3)*rad)
+!O      y(3) = 0.0D0
+!O      IF (iz1(3).EQ.1) THEN
+!O        z(3) = blen(3)*COS(alph(3)*rad)
+!O      ELSE
+!O        z(3) = z(2) - blen(3)*COS(alph(3)*rad)
+!O      ENDIF
+!O! As long as atoms remain linear with the first
+!O! two atoms, keep placing them along the z-axis
+!O      i = 3
+!O      IF (n.GT.3) THEN
+!O        DO WHILE (NINT(x(i)*10000).EQ.0)
+!O          i = i + 1
+!O          i1 = iz1(i)
+!O          i2 = iz2(i)
+!O          IF (z(i1).GT.z(i2)) THEN
+!O            sign = 1.0D0
+!O          ELSE
+!O            sign = -1.0D0
+!O          ENDIF
+!O          x(i) = blen(i)*SIN(alph(i)*rad)
+!O          y(i) = 0.0D0
+!O          z(i) = z(i1) - sign*blen(i)*COS(alph(i)*rad)
+!O        ENDDO
+!O      ENDIF
+!O! Loop over each atom in turn, finding its coordinates
+!O      k = i + 1
+!O      IF (k.LE.n) THEN
+!O        DO i = k, n
+!O          i1 = iz1(i)
+!O          i2 = iz2(i)
+!O          i3 = iz3(i)
+!O          bond = blen(i)
+!O          angle1 = alph(i)
+!O          angle2 = bet(i)
+!O          CALL xyzatm(x,y,z,i,i1,bond,i2,angle1,i3,angle2)
+!O        ENDDO
+!O      ENDIF
+!O  999 RETURN
+!O
+!O      END SUBROUTINE MAKEXYZ
+!O!*==XYZATM.f90  processed by SPAG 6.11Dc at 13:14 on 17 Sep 2001
+!O!
+!O!     "xyzatm" computes the Cartesian coordinates of a single
+!O!     atom from its defining internal coordinate values
+!O!
+!O      SUBROUTINE xyzatm(x,y,z,i,i1,bond,i2,angle1,i3,angle2)
+!O
+!O      IMPLICIT NONE
+!O!
+!O! Arguments
+!O      REAL*8 x(*), y(*), z(*)
+!O      REAL*8 bond, angle1, angle2
+!O      INTEGER i, i1, i2, i3
+!O! Constants
+!O      REAL*8 radian, pi
+!O      PARAMETER (radian=57.29577951308232088D0)
+!O      PARAMETER (pi=3.141592653589793238D0)
+!O      REAL*8 small
+!O      PARAMETER (small=1.D-8)
+!O! Local
+!O      REAL*8 ang_1, ang_2
+!O      REAL*8 sin_1, cos_1, sin_2, cos_2
+!O      REAL*8 cosine, one_over_sine, norm, eps, sinarg
+!O      REAL*8 u1(3), u2(3), u3(3), u4(3), rad
+!O!
+!O! convert the angle values from degrees to radians;
+!O! then find their sine and cosine values
+!O!
+!O      eps = 0.00000001D0
+!O      rad = 1.0/radian
+!O      ang_1 = angle1*rad
+!O      ang_2 = angle2*rad
+!O      sin_1 = SIN(ang_1)
+!O      cos_1 = COS(ang_1)
+!O      sin_2 = SIN(ang_2)
+!O      cos_2 = COS(ang_2)
+!O      u1(1) = x(i2) - x(i3)
+!O      u1(2) = y(i2) - y(i3)
+!O      u1(3) = z(i2) - z(i3)
+!O      norm = 1.0/SQRT(u1(1)*u1(1)+u1(2)*u1(2)+u1(3)*u1(3))
+!O      u1 = u1 * norm
+!O      u2(1) = x(i1) - x(i2)
+!O      u2(2) = y(i1) - y(i2)
+!O      u2(3) = z(i1) - z(i2)
+!O      norm = 1.0/SQRT(u2(1)*u2(1)+u2(2)*u2(2)+u2(3)*u2(3))
+!O      u2 = u2 * norm
+!O      u3(1) = u1(2)*u2(3) - u1(3)*u2(2)
+!O      u3(2) = u1(3)*u2(1) - u1(1)*u2(3)
+!O      u3(3) = u1(1)*u2(2) - u1(2)*u2(1)
+!O! Dot product of u1 and u2. As |u1| = |u2| = 1, this is the cosine of the angle between u1 and u2
+!O      cosine = u1(1)*u2(1) + u1(2)*u2(2) + u1(3)*u2(3)
+!O! Now there follows a test if the cosine of an angle > 1.0
+!O! I have to brush up on my mathematics here: I would never have added this test.
+!O      IF (ABS(cosine).LT.1.0D0) THEN
+!O        one_over_sine = 1.0/SQRT(1.0D0-cosine**2)
+!O      ELSE
+!O        sinarg = DMAX1(small,cosine**2-1.0D0)
+!O        one_over_sine = 1.0/SQRT(sinarg)
+!O      ENDIF
+!O      u3 = u3 * one_over_sine
+!O      u4(1) = u3(2)*u2(3) - u3(3)*u2(2)
+!O      u4(2) = u3(3)*u2(1) - u3(1)*u2(3)
+!O      u4(3) = u3(1)*u2(2) - u3(2)*u2(1)
+!O      x(i) = x(i1) + bond*(-u2(1)*cos_1 + u4(1)*sin_1*cos_2 + u3(1)*sin_1*sin_2)
+!O      y(i) = y(i1) + bond*(-u2(2)*cos_1 + u4(2)*sin_1*cos_2 + u3(2)*sin_1*sin_2)
+!O      z(i) = z(i1) + bond*(-u2(3)*cos_1 + u4(3)*sin_1*cos_2 + u3(3)*sin_1*sin_2)
+!O
+!O      END SUBROUTINE XYZATM
 !
 !*****************************************************************************
 !
