10c005b5ebf299f6c6c1919a8852080cea7f99dc
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2001-11-21 10:19:18 +0000

    General clean up

--- valchipro.f90
+++ valchipro.f90
@@ -6,17 +6,41 @@
       USE WINTERACTER
       USE DRUID_HEADER
 
+      IMPLICIT NONE
+
+      REAL, INTENT (  OUT) :: chivalpro
+
       INCLUDE 'PARAMS.INC'
       INCLUDE 'GLBVAR.INC'
       INCLUDE 'statlog.inc'
 
-      COMMON /FCSPEC/ NLGREF,IREFH(3,MFCSPE),LOGREF(8,MFCSPE)
-      COMMON /FCSTOR/MAXK,FOB(150,MFCSTO)
-      COMMON /SAREFLNS/AIOBS(MSAREF),AICALC(MSAREF)
-      COMMON /CHISTOP/ NOBS,NFIT,IFIT(MCHSTP),CHIOBS,&
-        WT(MCHSTP),XOBS(MCHSTP),YOBS(MCHSTP),YCAL(MCHSTP),ESD(MCHSTP)
-      COMMON /chibest/ ycalbest(MCHSTP)
-      COMMON /FPINF1/ KREFT(MFPINF),KNIPT(50,MFPINF),PIKVAL(50,MFPINF)
+      INTEGER         NLGREF, IREFH
+      LOGICAL                                  LOGREF
+      COMMON /FCSPEC/ NLGREF, IREFH(3,MFCSPE), LOGREF(8,MFCSPE)
+
+      INTEGER         MAXK
+      REAL                  FOB
+      COMMON /FCSTOR/ MAXK, FOB(150,MFCSTO)
+
+      REAL              AIOBS,         AICALC
+      COMMON /SAREFLNS/ AIOBS(MSAREF), AICALC(MSAREF)
+
+      INTEGER          NFITA, IFITA
+      REAL                                   CHIOBSA, WTSA
+      REAL             YCALA
+      COMMON /CHISTOP/ NFITA, IFITA(MCHSTP), CHIOBSA, WTSA(MCHSTP),    &
+                       YCALA(MCHSTP)
+
+      INTEGER          NBIN, LBIN
+      REAL                         XBIN,       YOBIN,       YCBIN,       YBBIN,       EBIN
+      COMMON /PROFBIN/ NBIN, LBIN, XBIN(MOBS), YOBIN(MOBS), YCBIN(MOBS), YBBIN(MOBS), EBIN(MOBS)
+
+      INTEGER         KREFT,         KNIPT
+      REAL                                             PIKVAL
+      COMMON /FPINF1/ KREFT(MFPINF), KNIPT(50,MFPINF), PIKVAL(50,MFPINF)
+
+      REAL SUM1, SUM2, YCALC, RESCL, CVP
+      INTEGER II, I, K, KK
 
 ! JvdS VALCHIPRO, which calculates the profile chi-squared, is always
 ! called after VALCHI. VALCHI already fills AICALC in COMMON /SAREFLNS/ (the COMMON block needs
@@ -24,33 +48,26 @@
   !    INCLUDE 'AllFFCalc.inc'
       SUM1 = 0.0
       SUM2 = 0.0
-      DO II = 1, NFIT
-        I = IFIT(II)
+      DO II = 1, NFITA
+        I = IFITA(II)
         YCALC = 0.0
         DO K = 1, KREFT(I)
           KK = KNIPT(K,I)
-          YCALC = YCALC+AICALC(KK)*PIKVAL(K,I)
+          YCALC = YCALC + AICALC(KK) * PIKVAL(K,I)
         ENDDO
-!!        DO K=KMINST(I),KMAXST(I)
-!!          KD=K-KMINST(I)
-!!          YCALC=YCALC+AICALC(K)*PIK(KD,I)
-!!        END DO
-!        WY=WT(I)*YCALC
-!        SUM1=SUM1+WY*YCALC
-!        SUM2=SUM2+WY*YOBS(I)
-        YCAL(I)=YCALC
-        SUM1=SUM1+YCALC
-        SUM2=SUM2+YOBS(I)
+        YCBIN(I) = YCALC
+        SUM1 = SUM1 + YCALC
+        SUM2 = SUM2 + YOBIN(I)
       ENDDO
       RESCL = SUM2 / SUM1
       CVP = 0.0
-      DO II = 1, NFIT
-        I = IFIT(II)
-        CVP = CVP+WT(I)*(YOBS(I)-RESCL*YCAL(I))**2
-        ycalbest(I) = rescl*ycal(I)
+      DO II = 1, NFITA
+        I = IFITA(II)
+        YCBIN(I) = RESCL * YCBIN(I)
+        CVP = CVP + WTSA(I) * (YOBIN(I) - YCBIN(I))**2
       END DO
 !      CHIVALPRO=CHIOBS!-SUM2*SUM2/SUM1
-      CHIVALPRO = CVP/FLOAT(NFIT-2)
+      CHIVALPRO = CVP/FLOAT(NFITA-2)
 
       END SUBROUTINE VALCHIPRO
 !
