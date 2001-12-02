2c0dc1b468eb04024df995b0c28f81007531a35b
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2001-12-02 13:42:53 +0000

    General clean up

--- Dialog_Routines.f90
+++ Dialog_Routines.f90
@@ -17,16 +17,24 @@
                        XPGMINOLD, XPGMAXOLD, YPGMINOLD, YPGMAXOLD,   &
                        XGGMIN,    XGGMAX
 
-      atem1=max(abs(xpmin),abs(xpmax))
-!C>> JCC Dont continue if range is silly
-      if (atem1.LE.0.000000001) RETURN
+      LOGICAL, EXTERNAL :: FnPatternOK
+
       CALL PushActiveWindowID
       CALL WDialogSelect(IDD_Data_Properties)
-      atem2=0.0001*abs(xpmax-xpmin)
-      item1=0.5+alog10(atem1)
-      item2=max(0.,0.5-alog10(atem2))
-      item=2+item1+item2
-      chrfmt(1:2)='(F'
+      IF (.NOT. FnPatternOK()) THEN
+        CALL WDialogClearField(IDF_xmin)
+        CALL WDialogClearField(IDF_xmax)
+        CALL WDialogClearField(IDF_ymin)
+        CALL WDialogClearField(IDF_ymax)
+        CALL PopActiveWindowID
+        RETURN
+      ENDIF
+      atem1 = MAX(ABS(xpmin),ABS(xpmax))
+      atem2 = 0.0001 * ABS(xpmax-xpmin)
+      item1 = 0.5 + ALOG10(atem1)
+      item2 = MAX(0.,0.5-ALOG10(atem2))
+      item  = 2 + item1 + item2
+      chrfmt(1:2) = '(F'
       IF (item .GE. 10) THEN
         CALL IntegerToString(item,chrfmt(3:4),'(I2)')
         inext = 5
@@ -34,31 +42,31 @@
         CALL IntegerToString(item,chrfmt(3:3),'(I1)')
         inext = 4
       END IF
-      chrfmt(inext:inext)='.'
+      chrfmt(inext:inext) = '.'
       inext = inext + 1
       CALL IntegerToString(item2,chrfmt(inext:inext),'(I1)')
       inext = inext + 1
-      chrfmt(inext:inext)=')'
+      chrfmt(inext:inext) = ')'
       CALL WDialogPutReal(IDF_xmin,xpmin,chrfmt(1:inext))
       CALL WDialogPutReal(IDF_xmax,xpmax,chrfmt(1:inext))
-      atem1=max(abs(ypmin),abs(ypmax))
-      atem2=0.0001*abs(ypmax-ypmin)
-      item1=0.5+alog10(atem1)
-      item2=max(0.,0.5-alog10(atem2))
-      item=2+item1+item2
-      chrfmt(1:2)='(F'
-      if (item.ge.10) then
-        call IntegerToString(item,chrfmt(3:4),'(I2)')
-        inext=5
-      else
-        call IntegerToString(item,chrfmt(3:3),'(I1)')
-        inext=4
-      end if
-      chrfmt(inext:inext)='.'
-      inext=inext+1
-      call IntegerToString(item2,chrfmt(inext:inext),'(I1)')
-      inext=inext+1
-      chrfmt(inext:inext)=')'
+      atem1 = MAX(ABS(ypmin),ABS(ypmax))
+      atem2 = 0.0001 * ABS(ypmax-ypmin)
+      item1 = 0.5 + ALOG10(atem1)
+      item2 = MAX(0.,0.5-ALOG10(atem2))
+      item  = 2 + item1 + item2
+      chrfmt(1:2) = '(F'
+      IF (item.GE.10) THEN
+        CALL IntegerToString(item,chrfmt(3:4),'(I2)')
+        inext = 5
+      ELSE
+        CALL IntegerToString(item,chrfmt(3:3),'(I1)')
+        inext = 4
+      ENDIF
+      chrfmt(inext:inext) = '.'
+      inext = inext + 1
+      CALL IntegerToString(item2,chrfmt(inext:inext),'(I1)')
+      inext = inext + 1
+      chrfmt(inext:inext) = ')'
       CALL WDialogPutReal(IDF_ymin,ypmin,chrfmt(1:inext))
       CALL WDialogPutReal(IDF_ymax,ypmax,chrfmt(1:inext))
       CALL PopActiveWindowID
