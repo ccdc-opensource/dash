49336490483e97ad18aa69a308770051a0df884c
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2002-01-15 15:27:29 +0000

    Orientation of a,b and c wrt orthogonal co-ordinates now same as CCSL

--- Basic_Vectors.f90
+++ Basic_Vectors.f90
@@ -5,20 +5,21 @@
 !
 !*****************************************************************************
 !
-      SUBROUTINE LatticeCellParameters2Lattice(a, b, c, alpha, beta, gamma, TheLattice)
+      SUBROUTINE LatticeCellParameters2Lattice(a, b, c, tAlpha, tBeta, tGamma, TheLattice)
 !
-! Calculates rigth-handed lattice from cell parameters, a along x, b in xy plane
+! Calculates right-handed lattice from cell parameters, c along z, b in yz plane
 !
 ! The left-most subscript must vary most rapidly
 !
       IMPLICIT NONE
 
-      REAL, INTENT (IN   ) :: a, b, c, alpha, beta, gamma
+      REAL, INTENT (IN   ) :: a, b, c, tAlpha, tBeta, tGamma
       REAL, INTENT (  OUT) :: TheLattice(1:3,1:3)
 
-      REAL    Degrees2Radians ! Function
-      LOGICAL ValidCellAxisLength ! Function
-      REAL    aSTAR(1:3)
+      REAL, EXTERNAL :: Degrees2Radians
+      LOGICAL, EXTERNAL :: ValidCellAxisLength
+      REAL    cSTAR(1:3)
+      REAL    alpha, beta, gamma ! to resolve call by value / call by reference conflict
 
       IF (.NOT. ValidCellAxisLength(a)) THEN
         CALL ErrorMessage('Invalid length of a axis')
@@ -32,30 +33,33 @@
         CALL ErrorMessage('Invalid length of c axis')
         RETURN
       ENDIF
-! a-axis
-      TheLattice(1,1) = a
-      TheLattice(2,1) = 0.0
-      TheLattice(3,1) = 0.0
-! b-axis
-      TheLattice(1,2) = b*COS(Degrees2Radians(gamma))
-      TheLattice(2,2) = b*SIN(Degrees2Radians(gamma))
-      TheLattice(3,2) = 0.0
+      alpha = Degrees2Radians(tAlpha)
+      beta  = Degrees2Radians(tBeta)
+      gamma = Degrees2Radians(tGamma)
 ! c-axis
-      TheLattice(1,3) = c*COS(Degrees2Radians(beta))
+      TheLattice(1,3) = 0.0
+      TheLattice(2,3) = 0.0
+      TheLattice(3,3) = c
+! b-axis
+      TheLattice(1,2) = 0.0
+      TheLattice(2,2) = b * SIN(alpha)
+      TheLattice(3,2) = b * COS(alpha)
+! a-axis
+      TheLattice(3,1) = a * COS(beta)
 ! Maybe an odd way to do this, but it works:
-! the projection of c on the a*b* plane equals the projection of c on the xy plane:
-! (ac)a* + (bc)b* = (cx)x + (cy)y
+! the projection of a on the "b*c*" plane equals the projection of a on the yz plane:
+! (ba)b* + (ca)c* = (ay)y + (az)z
 ! Multiplying from the right by y* yields:
-! (ac)(a*y) + (bc)(b*y) = cy
-! Note that x = x* and y = y*
-! Since b* = y/b, b*y = 1/b:
-! (ac)(a*y) + c(cos(alpha)) = cy
-! We know ac, bc and y and can calculate a*, leaving the unknown cy
-      aSTAR = TheLattice(:,1)
-      CALL VectorOrthogonalise(aSTAR,TheLattice(:,2))
-      CALL VectorSetLength(aSTAR,1.0/a)
-      TheLattice(2,3) = c*COS(Degrees2Radians(alpha)) + aSTAR(2) * (a*c*COS(Degrees2Radians(beta)))
-      TheLattice(3,3) = SQRT(c**2-TheLattice(1,3)**2-TheLattice(2,3)**2)
+! (ba)(b*y) + (ca)(c*y) = ay
+! Note that y = y* and z = z*
+! Since b* = y/|b|, b*y = 1/|b|:
+! a(cos(gamma)) + (ca)(c*y) = ay
+! We know ca and y and can calculate c*, leaving the unknown ay
+      cSTAR = TheLattice(:,3)
+      CALL VectorOrthogonalise(cSTAR,TheLattice(:,2))
+      CALL VectorSetLength(cSTAR,1.0/c)
+      TheLattice(2,1) = (c*a*COS(beta)) * cSTAR(2)   +   a * COS(gamma)
+      TheLattice(1,1) = SQRT(a**2-TheLattice(2,1)**2-TheLattice(3,1)**2)
 
       END SUBROUTINE LatticeCellParameters2Lattice
 !
