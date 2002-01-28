ba2d9be3a5db101e2093ae9a2490ad150ba78c62
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2002-01-28 11:17:16 +0000

    Variables for preferred orientation

---
+++ PO_VAR.f90
@@ -0,0 +1,27 @@
+!
+!*****************************************************************************
+!
+! This module contains all Preferred Orientation related data
+!
+      MODULE PO_VAR
+
+      LOGICAL PrefParExists
+
+! Determines if preferred orientation correction is to be included or not
+
+      INTEGER iPrfPar
+
+! The number of the SA parameter corresponding to the extent of preferred orientation
+
+      REAL PrefPars(1:4)
+
+! PrefPars(1:3) = Orientation (to be replaced by something called 'PO_Direction(1:3)
+! PrefPars(4)   = Extent of preferred orientation
+
+
+      REAL PrefCsqa(48,10000) ! 10000 = MSAREF
+
+      END MODULE PO_VAR
+!
+!*****************************************************************************
+!
