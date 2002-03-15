e452ab4f98371809d0f79b3d3018b710b16c6b33
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2002-03-15 14:27:25 +0000

    Added example strings to dialogue

--- Rietveld.f90
+++ Rietveld.f90
@@ -1,6 +1,19 @@
 !
 !*****************************************************************************
 !
+! Issues:
+!
+! We're probably going to include high angle data as well for the Rietveld Refinement,
+! which gives problems with the following items:
+!
+! - Background, both Monte Carlo and Polynomial
+! - Peak shape
+! - Cell parameters / zero point
+! - Maximum number of reflections ("350")?
+!
+!
+!*****************************************************************************
+!
       SUBROUTINE ShowWindowRietveld
 !
 ! The window containing the Rietveld Refinement needs a lot of initialisation,
@@ -14,6 +27,26 @@
 
       IMPLICIT NONE 
 
+      INTEGER iCol, iRow
+
+      iCol = 1
+      iRow = 1
+      CALL WDialogSelect(IDD_RRsubZmatrices)
+      CALL WGridPutCellString(IDF_RR_ZmatrixGrid,1,iRow,'x(frag1)')
+      CALL WGridPutCellReal(IDF_RR_ZmatrixGrid,2,iRow,0.5)
+      CALL WDialogSelect(IDD_RRsubBonds)
+      CALL WGridPutCellString(IDF_RR_BondGrid,1,iRow,'C1:C2')
+      CALL WGridPutCellReal(IDF_RR_BondGrid,2,iRow,1.54)
+      CALL WDialogSelect(IDD_RRsubAngles)
+      CALL WGridPutCellString(IDF_RR_AngleGrid,1,iRow,'C1:C2:C3')
+      CALL WGridPutCellReal(IDF_RR_AngleGrid,2,iRow,112.0)
+      CALL WDialogSelect(IDD_RRsubTorsions)
+      CALL WGridPutCellString(IDF_RR_TorsionGrid,1,iRow,'C1:C2:C3:C4')
+      CALL WGridPutCellReal(IDF_RR_TorsionGrid,2,iRow,180.0)
+      CALL WDialogSelect(IDD_RRsubAtoms)
+      CALL WGridPutCellString(IDF_RR_AtomGrid,1,iRow,'C1')
+      CALL WGridPutCellReal(IDF_RR_AtomGrid,2,iRow,3.0)
+      CALL WGridPutCellReal(IDF_RR_AtomGrid,4,iRow,1.0)
       CALL WDialogSelect(IDD_Rietveld)
       CALL WDialogShow(-1,-1,0,ModeLess)
 
@@ -38,7 +71,7 @@
           SELECT CASE (EventInfo%VALUE1)
             CASE (IDB_Refine)
 
-
+            CASE (IDB_Undo)
 
             CASE (IDCANCEL, IDCLOSE)
               CALL WDialogHide()
@@ -52,9 +85,6 @@
 
           END SELECT
         CASE (FieldChanged)
-
-
-
       END SELECT
       CALL PopActiveWindowID
 
