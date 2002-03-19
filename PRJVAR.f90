7a7cf99d40a33ba142ec28e7e9b2f246ac664d5e
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2002-03-19 12:13:27 +0000

    Module containing Project file related variables

---
+++ PRJVAR.f90
@@ -0,0 +1,23 @@
+!
+!*****************************************************************************
+!
+! This module contains all project file related data
+!
+      MODULE PRJVAR
+
+      IMPLICIT NONE
+
+      INTEGER hPrjFile
+      INTEGER iPrjRecNr
+      INTEGER iPrjReadOrWrite
+  
+      INTEGER     cWrite
+      PARAMETER ( cWrite = 0)
+
+      INTEGER     cRead
+      PARAMETER ( cRead = 1)
+
+      END MODULE PRJVAR
+!
+!*****************************************************************************
+!
