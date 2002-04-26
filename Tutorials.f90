c399d87b4e741b2581fb7b6832cdee46140aa7b3
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2002-04-26 14:19:01 +0000

    Copying data files now more elegant

--- Tutorials.f90
+++ Tutorials.f90
@@ -12,12 +12,9 @@
       INTEGER, INTENT (IN   ) :: Tutorial_ID
 
       CHARACTER(MaxPathLength) DirString, FileDir
-      CHARACTER(MaxPathLength) Files(100)
       CHARACTER(MaxPathLength) CurDir
-      INTEGER                  NFiles, I
       CHARACTER(1)             NumberStr
 
-      NFiles = 100
       CALL IOsDirName(CurDir)
       SELECT CASE (Tutorial_ID)
         CASE (ID_Tutorial_1)
@@ -33,11 +30,10 @@
       END SELECT
       DirString = InstallationDirectory(1:LEN_TRIM(InstallationDirectory))//"Documentation"//DIRSPACER//"Tutorial"//NumberStr
       FileDir = DirString(1:LEN_TRIM(DirString))//DIRSPACER//"Data files"
-      CALL IOsDirList(FileDir(1:LEN_TRIM(FileDir)),"",Files,Nfiles)
       CALL IOsDirChange(FileDir(1:LEN_TRIM(FileDir)))
-      DO I = 1, Nfiles
-        CALL IOsCopyFile(Files(I)(1:LEN_TRIM(Files(I))),CURDIR(1:LEN_TRIM(CURDIR))//DIRSPACER)
-      ENDDO
+      CALL IOsCopyFile('Tutorial_'//NumberStr//'.xye',CurDir(1:LEN_TRIM(CurDir))//DIRSPACER)
+      CALL IOsCopyFile('Tutorial_'//NumberStr//'*.mol2',CurDir(1:LEN_TRIM(CurDir))//DIRSPACER)
+      CALL IOsCopyFile('Tutorial_'//NumberStr//'*.zmatrix',CurDir(1:LEN_TRIM(CurDir))//DIRSPACER)
       CALL IOsDirChange(CurDir(1:LEN_TRIM(CurDir)))
       CALL WHelpFile(' ') ! In case the help file is open already
       CALL WHelpFile(DirString(1:LEN_TRIM(DirString))//DIRSPACER//"Tutorial"//NumberStr//".chm")
