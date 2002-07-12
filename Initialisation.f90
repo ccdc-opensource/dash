411de1a27dbaad83902ad1f1022fc81b59bafa89
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2002-07-12 11:44:52 +0000

    Added code to write out solution files after the SA has finished

--- Initialisation.f90
+++ Initialisation.f90
@@ -91,7 +91,8 @@
       CALL WDialogLoad(IDD_Summary)
       CALL WDialogLoad(IDD_SAW_Page5)
       CALL WDialogLoad(IDD_Parameter_Status_2)
-      CALL WDialogLoad(IDD_Rietveld)
+      CALL WDialogLoad(IDD_OutputSolutions)
+!      CALL WDialogLoad(IDD_Rietveld)
 
       END SUBROUTINE PolyFitter_UploadDialogues
 !
