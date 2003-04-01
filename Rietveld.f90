3c57e48c7c1142cb1d8a7bb9af3d93d0ab557657
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2003-04-01 07:42:39 +0000

    Added code to calculate atomic co-ordinates from Rietveld variables.

--- Rietveld.f90
+++ Rietveld.f90
@@ -114,17 +114,17 @@
             ! Rotations
             IF (natoms(iFrg) .NE. 1) THEN
               CALL WGridLabelRow(iField, iRow, 'Q0')
-              CALL WGridPutCellReal(iField, iCol, iRow, RR_rot(1,iFrg,iFrgCopy), "(F7.5)")
+              CALL WGridPutCellReal(iField, iCol, iRow, RR_rot(1,iFrg,iFrgCopy), "(F8.5)")
               iRow = iRow + 1
               CALL WGridLabelRow(iField, iRow, 'Q1')
-              CALL WGridPutCellReal(iField, iCol, iRow, RR_rot(2,iFrg,iFrgCopy), "(F7.5)")
+              CALL WGridPutCellReal(iField, iCol, iRow, RR_rot(2,iFrg,iFrgCopy), "(F8.5)")
               iRow = iRow + 1
               IF (UseQuaternions(iFrg)) THEN
                 CALL WGridLabelRow(iField, iRow, 'Q2')
-                CALL WGridPutCellReal(iField, iCol, iRow, RR_rot(3,iFrg,iFrgCopy), "(F7.5)")
+                CALL WGridPutCellReal(iField, iCol, iRow, RR_rot(3,iFrg,iFrgCopy), "(F8.5)")
                 iRow = iRow + 1
                 CALL WGridLabelRow(iField, iRow, 'Q3')
-                CALL WGridPutCellReal(iField, iCol, iRow, RR_rot(4,iFrg,iFrgCopy), "(F7.5)")
+                CALL WGridPutCellReal(iField, iCol, iRow, RR_rot(4,iFrg,iFrgCopy), "(F8.5)")
                 iRow = iRow + 1
               ENDIF
             ENDIF
@@ -203,6 +203,8 @@
       DO iRow = 1, nRows
         CALL WGridPutCellCheckBox(iField, iCol, iRow, 0)
       ENDDO
+      ! Initialise RR_AtmCoords(1:3,1:150)
+      CALL RR_MAKEFRAC
       CALL WDialogShow(-1,-1,0,ModeLess)
 
       END SUBROUTINE ShowWindowRietveld
@@ -214,10 +216,25 @@
       USE WINTERACTER
       USE DRUID_HEADER
       USE VARIABLES
-      USE ZMVAR
       USE RRVAR
+      USE SOLVAR
 
       IMPLICIT NONE      
+
+      INTEGER         NATOM
+      REAL                   X
+      INTEGER                          KX
+      REAL                                        AMULT,      TF
+      INTEGER         KTF
+      REAL                      SITE
+      INTEGER                              KSITE,      ISGEN
+      REAL            SDX,        SDTF,      SDSITE
+      INTEGER                                             KOM17
+      COMMON /POSNS / NATOM, X(3,150), KX(3,150), AMULT(150), TF(150),  &
+     &                KTF(150), SITE(150), KSITE(150), ISGEN(3,150),    &
+     &                SDX(3,150), SDTF(150), SDSITE(150), KOM17
+
+      INTEGER I, J
 
       CALL PushActiveWindowID
       CALL WDialogSelect(IDD_Rietveld2)
@@ -229,9 +246,15 @@
             CASE (IDCANCEL, IDCLOSE)
               CALL WDialogHide
             CASE (IDB_View)
-
-
-
+! Calls subroutine which opens Mercury window with .pdb file
+              ! Fill XAtmCoords(1:3,1:150,0)
+              DO I = 1, NATOM
+                DO J = 1, 3
+                  XAtmCoords(J,I,0) = RR_AtmCoords(J,I)
+                ENDDO
+              ENDDO
+              CALL SA_STRUCTURE_OUTPUT_PDB(0)
+              CALL ViewStructure('SA_best.pdb')
           END SELECT
         CASE (FieldChanged)
       END SELECT
@@ -246,6 +269,224 @@
 
 
       END SUBROUTINE RietveldRefinement
+!
+!*****************************************************************************
+!
+! Fills REAL RR_AtmCoords(1:3,1:150) using variables from RRVAR
+!
+! For the refinement, this should be preceded by a step that translates the
+! list of parameters to RR_tran, RR_rot etc.
+      SUBROUTINE RR_MAKEFRAC
+
+      USE VARIABLES
+      USE ZMVAR
+      USE RRVAR
+
+      IMPLICIT NONE
+
+      INTEGER           TotNumOfAtoms, NumOfHydrogens, NumOfNonHydrogens, OrderedAtm
+      COMMON  /ORDRATM/ TotNumOfAtoms, NumOfHydrogens, NumOfNonHydrogens, OrderedAtm(1:MAXATM)
+
+      INTEGER KATOM, i, KI
+      INTEGER iFrg, iFrgCopy
+      REAL CKK1, CKK2, CKK3, TRAN(1:3)
+      REAL QQSUM, QDEN, QUATER(0:3), tQ(0:3), ROTA(1:3,1:3), CART(1:3,1:MAXATM), Duonion(0:1)
+      INTEGER JQ, ICFRG
+      REAL tX, tY, tZ, XC, YC, ZC
+      LOGICAL, EXTERNAL :: Get_UseCrystallographicCoM
+
+      KATOM = 0
+! Loop over all the fragments
+      DO iFrg = 1, maxfrg
+        IF (gotzmfile(iFrg)) THEN
+          DO iFrgCopy = 1, zmNumberOfCopies(iFrg)
+            ! Position centre of mass inside unit cell
+            RR_tran(1,iFrg,iFrgCopy) = RR_tran(1,iFrg,iFrgCopy) - INT(RR_tran(1,iFrg,iFrgCopy))
+            RR_tran(2,iFrg,iFrgCopy) = RR_tran(2,iFrg,iFrgCopy) - INT(RR_tran(2,iFrg,iFrgCopy))
+            RR_tran(3,iFrg,iFrgCopy) = RR_tran(3,iFrg,iFrgCopy) - INT(RR_tran(3,iFrg,iFrgCopy))
+            CKK1 = RR_tran(1,iFrg,iFrgCopy)
+            CKK2 = RR_tran(2,iFrg,iFrgCopy)
+            CKK3 = RR_tran(3,iFrg,iFrgCopy)
+            TRAN(1) = CKK1*SNGL(F2CMAT(1,1)) + CKK2*SNGL(F2CMAT(1,2)) + CKK3*SNGL(F2CMAT(1,3))
+            TRAN(2) = CKK1*SNGL(F2CMAT(2,1)) + CKK2*SNGL(F2CMAT(2,2)) + CKK3*SNGL(F2CMAT(2,3))
+            TRAN(3) = CKK1*SNGL(F2CMAT(3,1)) + CKK2*SNGL(F2CMAT(3,2)) + CKK3*SNGL(F2CMAT(3,3))
+! If more than one atom then proceed
+            IF (natoms(iFrg).GT.1) THEN
+! If we have at least two atoms, there are two options:
+! 1. Rotate the whole molecule freely, using quaternions
+! 2. Specify the rotation axis (e.g. if molecule on mirror plane)
+              IF (UseQuaternions(iFrg)) THEN
+                QQSUM = 0.0
+                DO JQ = 1, 4
+                  QQSUM = QQSUM + RR_rot(JQ,iFrg,iFrgCopy)**2
+                ENDDO
+! QQSUM now holds the sum of the squares of the quaternions
+                QDEN = 1.0 / SQRT(QQSUM)
+                DO JQ = 0, 3
+                  QUATER(JQ) = QDEN * RR_rot(JQ+1,iFrg,iFrgCopy)
+                ENDDO
+! QUATER now holds the normalised quaternions
+                CALL RR_ROTMAK(QUATER,ROTA)
+! ROTA now holds the 3x3 rotation matrix corresponding to the quaternions
+              ELSE
+! Single axis, so we use the 2D analogue of quaternions: a complex number of length 1.0
+                Duonion(0) = RR_rot(1,iFrg,iFrgCopy)
+                Duonion(1) = RR_rot(2,iFrg,iFrgCopy)
+                QDEN = 1.0 / SQRT(Duonion(0)**2 + Duonion(1)**2)
+                Duonion(0) = Duonion(0) * QDEN 
+                Duonion(1) = Duonion(1) * QDEN 
+                QUATER(0) = Duonion(0) * zmSingleRotationQs(0,iFrg)
+                QUATER(1) = Duonion(1) * zmSingleRotationQs(1,iFrg)
+                QUATER(2) = Duonion(1) * zmSingleRotationQs(2,iFrg)
+                QUATER(3) = Duonion(1) * zmSingleRotationQs(3,iFrg)
+! QUATER now holds the normalised quaternion corresponding to the single rotation axis
+! Now premultiply with the original molecular orientation (JvdS I don't understand why
+! they must be premultiplied: I would say they should be postmultiplied)
+                tQ(0) =   QUATER(0)*zmInitialQs(0,iFrg) - QUATER(1)*zmInitialQs(1,iFrg) &
+                        - QUATER(2)*zmInitialQs(2,iFrg) - QUATER(3)*zmInitialQs(3,iFrg)
+                tQ(1) =   QUATER(0)*zmInitialQs(1,iFrg) + QUATER(1)*zmInitialQs(0,iFrg) &
+                        - QUATER(2)*zmInitialQs(3,iFrg) + QUATER(3)*zmInitialQs(2,iFrg)
+                tQ(2) =   QUATER(0)*zmInitialQs(2,iFrg) + QUATER(1)*zmInitialQs(3,iFrg) &
+                        + QUATER(2)*zmInitialQs(0,iFrg) - QUATER(3)*zmInitialQs(1,iFrg)
+                tQ(3) =   QUATER(0)*zmInitialQs(3,iFrg) - QUATER(1)*zmInitialQs(2,iFrg) &
+                        + QUATER(2)*zmInitialQs(1,iFrg) + QUATER(3)*zmInitialQs(0,iFrg)
+                CALL RR_ROTMAK(tQ,ROTA)
+! ROTA now holds the 3x3 rotation matrix corresponding to the single rotation axis
+              ENDIF
+            ENDIF
+            CALL RR_MAKEXYZ(natoms(iFrg),RR_blen(1,iFrg,iFrgCopy),RR_alph(1,iFrg,iFrgCopy),RR_bet(1,iFrg,iFrgCopy),        &
+                           IZ1(1,iFrg),IZ2(1,iFrg),IZ3(1,iFrg),CART)
+! Determine origin for rotations
+            ICFRG = ICOMFLG(iFrg)
+! If user set centre of mass flag to 0, then use the molecule's centre of mass
+            IF (ICFRG.EQ.0) THEN
+              XC = 0.0
+              YC = 0.0
+              ZC = 0.0
+              IF (Get_UseCrystallographicCoM()) THEN
+                DO I = 1, natoms(iFrg)
+                  XC = XC + AtomicWeighting(I,iFrg)*CART(1,I)
+                  YC = YC + AtomicWeighting(I,iFrg)*CART(2,I)
+                  ZC = ZC + AtomicWeighting(I,iFrg)*CART(3,I)
+                ENDDO
+              ELSE
+                DO I = 1, natoms(iFrg)
+                  XC = XC + CART(1,I)
+                  YC = YC + CART(2,I)
+                  ZC = ZC + CART(3,I)
+                ENDDO
+                XC = XC/FLOAT(natoms(iFrg))
+                YC = YC/FLOAT(natoms(iFrg))
+                ZC = ZC/FLOAT(natoms(iFrg))
+              ENDIF
+! Otherwise, use atom number ICFRG
+            ELSE
+              XC = CART(1,ICFRG)
+              YC = CART(2,ICFRG)
+              ZC = CART(3,ICFRG)
+            ENDIF
+! Subtract the origin from all atom positions
+            DO I = 1, natoms(iFrg)
+              CART(1,I) = CART(1,I) - XC
+              CART(2,I) = CART(2,I) - YC
+              CART(3,I) = CART(3,I) - ZC
+            ENDDO
+! Apply rotation and translation to the atoms of this Z-matrix
+            CALL RR_DO_ATOM_POS(TRAN,ROTA,CART,natoms(iFrg))
+! When we are here, we have the actual co-ordinates of all the atoms in this Z-matrix
+! in Cartesian (orthogonal) co-ordinates. We need fractional co-ordinates: convert.
+            DO I = 1, natoms(iFrg)
+              tX = CART(1,I)*SNGL(c2fmat(1,1)) + CART(2,I)*SNGL(c2fmat(1,2)) + CART(3,I)*SNGL(c2fmat(1,3))
+              tY = CART(1,I)*SNGL(c2fmat(2,1)) + CART(2,I)*SNGL(c2fmat(2,2)) + CART(3,I)*SNGL(c2fmat(2,3))
+              tZ = CART(1,I)*SNGL(c2fmat(3,1)) + CART(2,I)*SNGL(c2fmat(3,2)) + CART(3,I)*SNGL(c2fmat(3,3))
+              KI = KATOM + I
+! Note that we must reorder the atoms such that the hydrogens are appended after the 
+! non-hydrogens.
+              RR_AtmCoords(1,OrderedAtm(KI)) = tX
+              RR_AtmCoords(2,OrderedAtm(KI)) = tY
+              RR_AtmCoords(3,OrderedAtm(KI)) = tZ
+            ENDDO
+            KATOM = KATOM + natoms(iFrg)
+          ENDDO
+        ENDIF
+      ENDDO
+
+      END SUBROUTINE RR_MAKEFRAC
+!
+!*****************************************************************************
+!
+      SUBROUTINE RR_ROTMAK(DC4,ROTA)
+!
+! Converts 4 quaternions to a 3x3 rotation matrix
+!
+      IMPLICIT NONE
+
+      REAL DC4(0:3), ROTA(1:3,1:3)
+
+      REAL EL(0:3,0:3)
+      INTEGER I, J
+
+      DO I = 0, 3
+        DO J = I, 3
+          EL(I,J) = 2.0 * DC4(I) * DC4(J)
+        ENDDO
+      ENDDO
+      ROTA(1,1) = 0.5 * (EL(0,0) + EL(1,1) - EL(2,2) - EL(3,3))
+      ROTA(2,2) = 0.5 * (EL(0,0) - EL(1,1) + EL(2,2) - EL(3,3))
+      ROTA(3,3) = 0.5 * (EL(0,0) - EL(1,1) - EL(2,2) + EL(3,3))
+      ROTA(1,2) = EL(1,2) + EL(0,3)
+      ROTA(1,3) = EL(1,3) - EL(0,2)
+      ROTA(2,1) = EL(1,2) - EL(0,3)
+      ROTA(2,3) = EL(2,3) + EL(0,1)
+      ROTA(3,1) = EL(1,3) + EL(0,2)
+      ROTA(3,2) = EL(2,3) - EL(0,1)
+
+      END SUBROUTINE RR_ROTMAK
+!
+!*****************************************************************************
+!
+      SUBROUTINE RR_DO_ATOM_POS(TRANS,ROTA,POS,NATOMS)
+
+      IMPLICIT NONE
+
+      REAL,  INTENT (IN   ) :: TRANS(3), ROTA(3,3)
+      REAL,  INTENT (  OUT) :: POS(3,*)
+      INTEGER, INTENT (IN   ) :: NATOMS
+
+      REAL POSIN(3)
+      INTEGER I, J
+
+      DO J = 1, NATOMS
+        DO I = 1, 3
+          POSIN(I) = POS(I,J)
+        ENDDO
+        CALL RR_ROTCAR(POSIN,POS(1,J),ROTA)
+        DO I = 1, 3
+          POS(I,J) = POS(I,J) + TRANS(I)
+        ENDDO
+      ENDDO
+
+      END SUBROUTINE RR_DO_ATOM_POS
+!
+!*****************************************************************************
+!
+      SUBROUTINE RR_ROTCAR(XORTO,XORTN,ROTA)
+
+      IMPLICIT NONE
+
+      REAL,  INTENT (IN   ) :: XORTO(3), ROTA(3,3)
+      REAL,  INTENT (  OUT) :: XORTN(3)
+
+      INTEGER I, J
+
+      DO I = 1, 3
+        XORTN(I) = 0.0
+        DO J = 1, 3
+          XORTN(I) = XORTN(I) + ROTA(I,J)*XORTO(J)
+        ENDDO
+      ENDDO
+
+      END SUBROUTINE RR_ROTCAR
 !
 !*****************************************************************************
 !
--- Vicar.f90
+++ Vicar.f90
@@ -151,4 +151,152 @@
 !
 !*****************************************************************************
 !
+! Same as above but for Rietveld refinement. Uses REAL rather than REAL*8
+!
+      SUBROUTINE RR_makexyz(n,blen,alph,bet,iz1,iz2,iz3,Cartesian)
+
+      IMPLICIT NONE
+
+      INTEGER, INTENT (IN   ) :: n ! Number of atoms
+      INTEGER iz1(*), iz2(*), iz3(*)
+      REAL blen(*), alph(*), bet(*), Cartesian(1:3,*)
+
+      REAL radian, pi, sqrtpi, twosix, logten
+      PARAMETER (radian=57.29577951308232088)
+      PARAMETER (pi=3.141592653589793238)
+      PARAMETER (sqrtpi=1.772453850905516027)
+      PARAMETER (twosix=1.122462048309372981)
+      PARAMETER (logten=2.302585092994045684)
+
+! Local
+! replace radian with rad=(1/radian)
+      INTEGER i, k, i1, i2, i3
+      REAL bond, angle1, angle2, sign, rad
+
+      IF (N.LT.1) RETURN
+      rad = 1.0/radian
+! First atom is placed at the origin
+      Cartesian(1,1) = 0.0
+      Cartesian(2,1) = 0.0
+      Cartesian(3,1) = 0.0
+! Second atom is placed along the z-axis
+      IF (N.LT.2) GOTO 999
+      Cartesian(1,2) = 0.0
+      Cartesian(2,2) = 0.0
+      Cartesian(3,2) = blen(2)
+! Third atom is placed in the x,z-plane
+      IF (N.LT.3) GOTO 999
+      Cartesian(1,3) = blen(3)*SIN(alph(3)*rad)
+      Cartesian(2,3) = 0.0
+      IF (iz1(3).EQ.1) THEN
+        Cartesian(3,3) = blen(3)*COS(alph(3)*rad)
+      ELSE
+        Cartesian(3,3) = Cartesian(3,2) - blen(3)*COS(alph(3)*rad)
+      ENDIF
+! As long as atoms remain linear with the first
+! two atoms, keep placing them along the z-axis
+      i = 3
+      IF (N.GT.3) THEN
+        DO WHILE (NINT(Cartesian(1,i)*10000).EQ.0 .AND. i.LT.N)
+          i = i + 1
+          i1 = iz1(i)
+          i2 = iz2(i)
+          IF (Cartesian(3,i1).GT.Cartesian(3,i2)) THEN
+            sign = 1.0
+          ELSE
+            sign = -1.0
+          ENDIF
+          Cartesian(1,i) = blen(i)*SIN(alph(i)*rad)
+          Cartesian(2,i) = 0.0
+          Cartesian(3,i) = Cartesian(3,i1) - sign*blen(i)*COS(alph(i)*rad)
+        ENDDO
+      ENDIF
+! Loop over each atom in turn, finding its coordinates
+      k = i + 1
+      IF (k.LE.n) THEN
+        DO i = k, n
+          i1 = iz1(i)
+          i2 = iz2(i)
+          i3 = iz3(i)
+          bond = blen(i)
+          angle1 = alph(i)
+          angle2 = bet(i)
+          CALL RR_xyzatm(Cartesian,i,i1,bond,i2,angle1,i3,angle2)
+        ENDDO
+      ENDIF
+  999 RETURN
+
+      END SUBROUTINE RR_makexyz
+!
+!*****************************************************************************
+!
+      SUBROUTINE RR_xyzatm(Cartesian,i,i1,bond,i2,angle1,i3,angle2)
+!
+!     "xyzatm" computes the Cartesian coordinates of a single
+!     atom from its defining internal coordinate values
+
+      IMPLICIT NONE
+!
+! Arguments
+      REAL Cartesian(1:3,*)
+      REAL bond, angle1, angle2
+      INTEGER i, i1, i2, i3
+! Constants
+      REAL radian, pi
+      PARAMETER (radian=57.29577951308232088)
+      PARAMETER (pi=3.141592653589793238)
+      REAL small
+      PARAMETER (small=1.E-8)
+! Local
+      REAL ang_1, ang_2
+      REAL sin_1, cos_1, sin_2, cos_2
+      REAL cosine, one_over_sine, norm, eps, sinarg
+      REAL u1(3), u2(3), u3(3), u4(3), rad
+!
+! convert the angle values from degrees to radians;
+! then find their sine and cosine values
+!
+      eps = 0.00000001
+      rad = 1.0/radian
+      ang_1 = angle1*rad
+      ang_2 = angle2*rad
+      sin_1 = SIN(ang_1)
+      cos_1 = COS(ang_1)
+      sin_2 = SIN(ang_2)
+      cos_2 = COS(ang_2)
+      u1(1) = Cartesian(1,i2) - Cartesian(1,i3)
+      u1(2) = Cartesian(2,i2) - Cartesian(2,i3)
+      u1(3) = Cartesian(3,i2) - Cartesian(3,i3)
+      norm = 1.0/SQRT(u1(1)*u1(1)+u1(2)*u1(2)+u1(3)*u1(3))
+      u1 = u1 * norm
+      u2(1) = Cartesian(1,i1) - Cartesian(1,i2)
+      u2(2) = Cartesian(2,i1) - Cartesian(2,i2)
+      u2(3) = Cartesian(3,i1) - Cartesian(3,i2)
+      norm = 1.0/SQRT(u2(1)*u2(1)+u2(2)*u2(2)+u2(3)*u2(3))
+      u2 = u2 * norm
+      u3(1) = u1(2)*u2(3) - u1(3)*u2(2)
+      u3(2) = u1(3)*u2(1) - u1(1)*u2(3)
+      u3(3) = u1(1)*u2(2) - u1(2)*u2(1)
+! Dot product of u1 and u2. As |u1| = |u2| = 1, this is the cosine of the angle between u1 and u2
+      cosine = u1(1)*u2(1) + u1(2)*u2(2) + u1(3)*u2(3)
+! Now there follows a test if the cosine of an angle > 1.0
+! I have to brush up on my mathematics here: I would never have added this test.
+      IF (ABS(cosine).LT.1.0) THEN
+        one_over_sine = 1.0/SQRT(1.0-cosine**2)
+      ELSE
+        sinarg = MAX1(small,cosine**2-1.0)
+        one_over_sine = 1.0/SQRT(sinarg)
+      ENDIF
+      u3 = u3 * one_over_sine
+      u4(1) = u3(2)*u2(3) - u3(3)*u2(2)
+      u4(2) = u3(3)*u2(1) - u3(1)*u2(3)
+      u4(3) = u3(1)*u2(2) - u3(2)*u2(1)
+      Cartesian(1,i) = Cartesian(1,i1) + bond*(-u2(1)*cos_1 + u4(1)*sin_1*cos_2 + u3(1)*sin_1*sin_2)
+      Cartesian(2,i) = Cartesian(2,i1) + bond*(-u2(2)*cos_1 + u4(2)*sin_1*cos_2 + u3(2)*sin_1*sin_2)
+      Cartesian(3,i) = Cartesian(3,i1) + bond*(-u2(3)*cos_1 + u4(3)*sin_1*cos_2 + u3(3)*sin_1*sin_2)
+
+      END SUBROUTINE RR_xyzatm
+!
+!*****************************************************************************
+!
 
