6ec00486b9122bede59a249bac6fb552c4c0aa44
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2003-09-10 15:49:46 +0000

    Removed variables PkFnVarVal and PkFnVarEsd which have now been replaced by PeakShapeSigma(1:2) etc.

--- multipeak_fitter.f90
+++ multipeak_fitter.f90
@@ -116,13 +116,11 @@
 
       REAL              PkFnVal,                      PkFnEsd,                      &
                         PkFnCal,                                                    &
-                        PkFnVarVal,                   PkFnVarEsd,                   &
                         PkAreaVal,                    PkAreaEsd,                    &
                         PkPosVal,                     PkPosEsd,                     &
                         PkPosAv
       COMMON /PEAKFIT2/ PkFnVal(MPkDes,Max_NPFR),     PkFnEsd(MPkDes,Max_NPFR),     &
                         PkFnCal(MPkDes,Max_NPFR),                                   &
-                        PkFnVarVal(3,MPkDes),         PkFnVarEsd(3,MPkDes),         &
                         PkAreaVal(MAX_NPPR,MAX_NPFR), PkAreaEsd(MAX_NPPR,MAX_NPFR), &
                         PkPosVal(MAX_NPPR,MAX_NPFR),  PkPosEsd(MAX_NPPR,MAX_NPFR),  &
                         PkPosAv(MAX_NPFR)
@@ -429,13 +427,11 @@
 
       REAL              PkFnVal,                      PkFnEsd,                      &
                         PkFnCal,                                                    &
-                        PkFnVarVal,                   PkFnVarEsd,                   &
                         PkAreaVal,                    PkAreaEsd,                    &
                         PkPosVal,                     PkPosEsd,                     &
                         PkPosAv
       COMMON /PEAKFIT2/ PkFnVal(MPkDes,Max_NPFR),     PkFnEsd(MPkDes,Max_NPFR),     &
                         PkFnCal(MPkDes,Max_NPFR),                                   &
-                        PkFnVarVal(3,MPkDes),         PkFnVarEsd(3,MPkDes),         &
                         PkAreaVal(MAX_NPPR,MAX_NPFR), PkAreaEsd(MAX_NPPR,MAX_NPFR), &
                         PkPosVal(MAX_NPPR,MAX_NPFR),  PkPosEsd(MAX_NPPR,MAX_NPFR),  &
                         PkPosAv(MAX_NPFR)
