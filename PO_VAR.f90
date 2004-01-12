b00d2afb040b6a0fd9b065bd7a21f8c311b51de1
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2004-01-12 09:05:51 +0000

    Added variables to store preferred orientation state from SA to enable restoring this state when the Rietveld window is closed.

--- PO_VAR.f90
+++ PO_VAR.f90
@@ -8,18 +8,24 @@
       IMPLICIT NONE
 
       LOGICAL PrefParExists
+! Determines if preferred orientation correction is to be included or not.
 
-! Determines if preferred orientation correction is to be included or not
+! Now that it is possible to switch on PO during RR, closing the RR window leaves the
+! program in an inconsistent state: SA does not use PO, but RR does. Therefore, when RR
+! window is closed, state must be reset to original state.
+
+      LOGICAL SA_PrefParExists
+      REAL    SA_PrefPars(1:3)
+! For use in Rietveld only, to enable restoring original state after closing Rietveld window.
 
       INTEGER iPrfPar
-
-! The number of the SA parameter corresponding to the extent of preferred orientation
+! The number of the SA parameter corresponding to the extent of preferred orientation.
 
       REAL PrefPars(1:3)
+! PrefPars(1:3) = Orientation (to be replaced by something called 'PO_Direction(1:3).
 
-! PrefPars(1:3) = Orientation (to be replaced by something called 'PO_Direction(1:3)
-
-      REAL PrefCsqa(48,10000) ! 10000 = MFCSTO
+      REAL PrefCsqa(48, 10000) ! 10000 = MFCSTO
+! Precalculated preferred orientation corrections (generalised multiplicities).
 
       END MODULE PO_VAR
 !
