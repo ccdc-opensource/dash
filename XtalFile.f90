84297ac67442b6099565e4e854fce3b4fe03c77a
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2004-04-24 11:56:20 +0000

    Added code to read in unit-cell parameters from a crystal structure file.

--- XtalFile.f90
+++ XtalFile.f90
@@ -69,9 +69,6 @@
 !
       SUBROUTINE XtalFileLoad(TheFileName)
 
-!C Note that the variables BLEN, ALPH and BET in ZMVAR do not allow storing of different
-!C variables for copies!. This has not been a problem yet, but this will become a problem!
-
       USE WINTERACTER
       USE DRUID_HEADER
       USE VARIABLES
@@ -276,6 +273,188 @@
       CLOSE(iHandle) 
 
       END SUBROUTINE XtalFileLoad
+!
+!*****************************************************************************
+!
+      SUBROUTINE UnitCellParametersFileBrowse
+!
+! This routine lets the user browse a directory for a crystal structure file.
+! If a valid file has been selected, it will be opened automatically.
+! Effectively, this routine is just a wrap around a file_open routine
+! such that it lets the user visually select a file first.
+!
+      USE WINTERACTER
+      USE VARIABLES
+
+      IMPLICIT NONE
+
+      CHARACTER(LEN=120) FILTER
+      INTEGER           IFLAGS, IFTYPE 
+      CHARACTER(LEN=MaxPathLength) tFileName
+
+      IFLAGS = LoadDialog + DirChange + PromptOn + AppendExt
+      FILTER = 'All files (*.*)|*.*|'//&
+               'Crystal structure files|*.cif;*.pdb;*.mol2;*.ml2;*.mol;*.mdl;*.ins;*.res;*.cssr|'
+      tFileName = ' '
+! IFTYPE specifies which of the file types in the list is the default
+      IFTYPE = 2
+      CALL WSelectFile(FILTER, IFLAGS, tFileName, 'Open crystal structure file', IFTYPE)
+! Did the user press cancel?
+      IF (WInfoDialog(ExitButtonCommon) .NE. CommonOK) RETURN
+! Note that up to this point, none of the global variables had changed. Baling out was no problem.
+! Try to open the file. This can be removed, of course, and relocated to places in the code where
+! the current subroutine is called.
+! Actually, that is how it works in practice under Windows (try 'Start' -> 'Run...' -> 'Browse...'
+! it will not actually open the file, just select it).
+      CALL UnitCellParametersFileOpen(tFileName)
+
+      END SUBROUTINE UnitCellParametersFileBrowse
+!
+!*****************************************************************************
+!
+      SUBROUTINE UnitCellParametersFileOpen(TheFileName)
+!
+! This routine tries to open a file containing a crystal structure.
+!
+! INPUT   : TheFileName = the file name
+!
+      USE WINTERACTER
+      USE VARIABLES
+
+      IMPLICIT NONE
+
+      CHARACTER*(*), INTENT (IN   ) :: TheFileName
+
+      LOGICAL FExists
+      INTEGER KLEN
+
+      KLEN = LEN_TRIM(TheFileName)
+      IF (KLEN .EQ. 0) RETURN
+      INQUIRE(FILE=TheFileName(1:KLEN),EXIST=FExists)
+      IF (.NOT. FExists) THEN
+        CALL ErrorMessage("The file "//TheFileName(1:KLEN)//" does not exist!")
+        RETURN
+      ENDIF
+      CALL UnitCellParametersFileLoad(TheFileName) 
+      
+      END SUBROUTINE UnitCellParametersFileOpen
+!
+!*****************************************************************************
+!
+      SUBROUTINE UnitCellParametersFileLoad(TheFileName)
+
+      USE WINTERACTER
+      USE DRUID_HEADER
+      USE VARIABLES
+
+      IMPLICIT NONE
+
+      INCLUDE 'LATTICE.INC'
+
+      CHARACTER*(*), INTENT (IN   ) :: TheFileName
+
+!      LOGICAL, EXTERNAL :: FnUnitCellOK
+      INTEGER, EXTERNAL :: CSSR2Mol2, proposed_crystal_system
+      INTEGER iHandle
+      CHARACTER(MaxPathLength) :: line
+      CHARACTER(MaxPathLength) :: tFileName
+      CHARACTER(MaxPathLength) :: globFile
+      CHARACTER(MaxPathLength) :: ExtensionStr
+      INTEGER nl, iStat, iStart
+      CHARACTER(20) KeyChar
+      CHARACTER(5) fmt
+      INTEGER I, ExtLen
+      INTEGER iLen, iPos
+
+      tFileName = TheFileName
+      iLen = LEN_TRIM(tFileName)
+! Find the last occurence of '.' in tFileName
+      iPos = iLen - 1 ! Last character of tFileName is not tested
+! The longest extension possible is five
+      DO WHILE ((iPos .NE. 0) .AND. (tFileName(iPos:iPos) .NE. '.') .AND. (iPos .NE. (iLen-5)))
+        iPos = iPos - 1
+      ENDDO
+! If we haven't found a '.' by now, we cannot deal with the extension anyway
+      IF (tFileName(iPos:iPos) .NE. '.') THEN
+        CALL ErrorMessage('Invalid extension.') 
+        RETURN
+      ENDIF
+      ExtensionStr = tFileName(iPos+1:iLen)
+      CALL ILowerCase(ExtensionStr)
+      SELECT CASE (ExtensionStr)
+        CASE ('cif ')
+          fmt = '-cif'
+        CASE ('cssr')
+          IF (CSSR2Mol2(tFileName) .NE. 1) RETURN
+! Replace 'cssr' by 'mol2'
+          tFileName = tFileName(1:iLen-4)//'mol2'
+          iLen = LEN_TRIM(tFileName)
+          fmt = '-mol2'
+        CASE ('res ', 'ins ')
+          fmt = '-res'
+        CASE ('pdb ')
+          fmt = '-pdb'
+        CASE ('mol2','ml2 ')
+          fmt = '-mol2'
+        CASE ('mol ','mdl ')
+          fmt = '-mol'
+      END SELECT
+! Run silently, 
+      CALL IOSDeleteFile('MakeZmatrix.log')
+      iStat = InfoError(1) ! Clear any errors 
+      iStart = 1
+      DO I = 1, iLen
+        IF (tFileName(I:I) .EQ. DIRSPACER) iStart = I + 1
+      ENDDO
+      CALL WCursorShape(CurHourGlass)
+      CALL IOSCommand(InstallationDirectory(1:LEN_TRIM(InstallationDirectory))//'zmconv.exe'// &
+        ' '//fmt(1:LEN_TRIM(fmt))//' "'//tFileName(iStart:iLen)//'" cell_only',3)
+        CALL WCursorShape(CurCrossHair)
+! Check return status
+      OPEN(UNIT=145, FILE='MakeZmatrix.log',STATUS='OLD',IOSTAT = iStat)
+      CLOSE(145)
+      IF ((InfoError(1) .EQ. ErrOSCommand) .OR. (iStat .NE. 0)) THEN
+        CALL ErrorMessage("Sorry, could not read unit-cell parameters.")
+        RETURN
+      ENDIF
+!C Replace extension by .glob
+      ExtLen = 5 ! Maximum length of a valid extension
+      CALL FileGetExtension(TheFileName, ExtensionStr, ExtLen)
+      IF ((ExtLen .LT. 1) .OR. (ExtLen .GT. 5)) THEN
+        CALL ErrorMessage("Error parsing file extension")
+        RETURN
+      ENDIF
+      iLen = LEN_TRIM(tFileName)
+      globFile = tFileName(1:iLen-ExtLen)//"glob"
+!C Load .glob file
+      iHandle = 10
+      OPEN(iHandle, FILE=globFile(1:LEN_TRIM(globFile)), STATUS='OLD', ERR=999)
+ 10   line = ''
+      READ(iHandle, '(A)', END=100, ERR=999) line
+      nl = LEN_TRIM(line)
+      CALL ILowerCase(line(1:nl))
+      CALL INextString(line,keychar)
+      SELECT CASE (KeyChar(1:3))
+        CASE ('cel')                                ! Cell parameters
+          DO I = 1, 6
+            CALL INextReal(line, CellPar(i))
+          ENDDO
+          LatBrav = proposed_crystal_system()
+          CALL Upload_CrystalSystem
+          ! Upload_CrystalSystem() calls UpdateCell()
+          ! UpdateCell() calls GenerateTickMarks()
+        CASE ('spa')
+     ! Ignore
+        CASE ('sym')
+     ! Ignore
+      END SELECT
+      GOTO 10 
+ 100  CLOSE(iHandle)
+      RETURN
+ 999  CALL ErrorMessage('Error reading crystal structure file.')
+      CLOSE(iHandle) 
+
+      END SUBROUTINE UnitCellParametersFileLoad
 !
 !*****************************************************************************
 !
