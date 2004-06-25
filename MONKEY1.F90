72223b9949f2908547ec6fe3a14bb07c24de58a6
Jacco van de Streek <jaccos@ccdc.cam.ac.uk>
2004-06-25 14:12:37 +0000

    Attempt to make MAIN and 3.0 release branch same.

--- MONKEY1.F90
+++ MONKEY1.F90
@@ -163,7 +163,7 @@
 
       INTEGER*4                  hKeyRoot
       CHARACTER*(512)            RegPath
-      CHARACTER*(4)              LatestVersion
+      CHARACTER*(6)              LatestVersion
       CHARACTER*(128)            TempValueName
       INTEGER*4                  hKey
       INTEGER*4                  retCode
@@ -185,13 +185,41 @@
       CHARACTER*MAX_DATA_LEN     bData
       INTEGER                    i, j
 
+      LOGICAL, EXTERNAL :: InfoMessage
+
 ! First attempt: HKEY_LOCAL_MACHINE\SOFTWARE\CCDC\
       cbValueName = MAX_VALUE_NAME
       dwcClassLen = MAX_PATH
       hKeyRoot = HKEY_LOCAL_MACHINE 
-
-!First step is to get version number of Mogul
+! Need to get latest version number of Mogul
       RegPath = 'SOFTWARE\CCDC\'//CHAR(0)
+! Will be more than one "latest version" in SOFTWARE CCDC (CSD for instance)
+! so need to get number of values of keys to search through (dwcValues)
+
+      retCode = RegOpenKeyEx(hKeyRoot,    &
+                             RegPath,     &
+                             0,           &
+                             KEY_EXECUTE, &
+                             LOC(hKey))
+      IF (retCode .NE. ERROR_SUCCESS) GOTO 20
+! ADD A QUERY AND ALLOCATE A BUFFER FOR BDATA.
+      retCode = RegQueryInfoKey(hKey,                 &  ! Key handle returned from RegOpenKeyEx.
+                                ClassName,            &  ! Buffer for class name.
+                                LOC(dwcClassLen),     &  ! Length of class string.
+                                NULL,                 &  ! Reserved.
+                                LOC(dwcSubKeys),      &  ! Number of sub keys.
+                                LOC(dwcMaxSubKey),    &  ! Longest sub key size.
+                                LOC(dwcMaxClass),     &  ! Longest class string.
+                                LOC(dwcValues),       &  ! Number of values for this key.
+                                LOC(dwcMaxValueName), &  ! Longest Value name.
+                                LOC(dwcMaxValueData), &  ! Longest Value data.
+                                LOC(dwcSecDesc),      &  ! Security descriptor.
+                                ftLastWriteTime)         ! Last write time.
+      i = RegCloseKey(hKey) ! Close the key handle.
+
+
+! Look through values of key
+      DO J = 1, dwcValues
 ! Use RegOpenKeyEx() with the new Registry path to get an open handle
 ! to the child key you want to enumerate.
       retCode = RegOpenKeyEx(hKeyRoot,    &
@@ -216,7 +244,8 @@
       IF (retCode .NE. ERROR_SUCCESS) GOTO 20
       cbData = dwcMaxValueData
 ! ENUMERATE THE KEY.
-      dwLBIndex = 0 ! I'm guessing here
+      dwLBIndex = J - 1
+      cbValueName = MAX_VALUE_NAME
       retCode = RegEnumValue (hKey,             & ! Key handle returned from RegOpenKeyEx.
                               dwLBIndex,        & ! Value index, taken from listbox.
                               ValueName,        & ! Name of value.
@@ -227,15 +256,17 @@
                               LOC(cbData))        ! Size of data buffer.
       IF (retCode .NE. ERROR_SUCCESS) GOTO 20
       IF (dwType .NE. REG_SZ) GOTO 20
+      
+      IF (ValueName(1:5) .EQ. 'Mogul') THEN
+        LatestVersion = TRIM(bData)
+        RegPath = 'SOFTWARE\CCDC\Mogul\'//LatestVersion(1:cbData)//'\'//CHAR(0) ! this seems to work//'\'//CHAR(0)
+        EXIT
+      ENDIF
       i = RegCloseKey(hKey) ! Close the key handle.
+      ENDDO
 
-! latest mogul version number in bdata
-      LatestVersion = TRIM(Bdata)
-
-      RegPath = 'SOFTWARE\CCDC\Mogul\'//LatestVersion(1:3)//'\'//CHAR(0) ! this seems to work//'\'//CHAR(0)
-
-
-!Will be more than one value (String) so need to get value for dcwValues
+!Looking for executable but will more than one directory listed (install, data etc)
+!so need to find number of values for key
       retCode = RegOpenKeyEx(hKeyRoot,    &
                              RegPath,     &
                              0,           &
@@ -283,7 +314,7 @@
 
 ! ENUMERATE THE KEY.
       dwLBIndex = J - 1
-      cbValueName = MAX_VALUE_NAME 
+      cbValueName = MAX_VALUE_NAME
       retCode = RegEnumValue (hKey,             & ! Key handle returned from RegOpenKeyEx.
                               dwLBIndex,        & ! Value index, taken from listbox.
                               ValueName,        & ! Name of value.
@@ -294,7 +325,6 @@
                               LOC(cbData))        ! Size of data buffer.
       IF (retCode .NE. ERROR_SUCCESS) GOTO 20
       IF (dwType .NE. REG_SZ) GOTO 20
-
       TempValueName = ValueName
       IF (TempValueName(1:3) .EQ. 'Exe') THEN
         MOGULEXE(1:cbData) = bData(1:cbData)
@@ -303,23 +333,24 @@
       i = RegCloseKey(hKey) ! Close the key handle.
       ENDDO
 
+20 CONTINUE
 
-
-! Second attempt: HKEY_CURRENT_USER\SOFTWARE\Mogul\
+! Second attempt: HKEY_CURRENT_USER\SOFTWARE\CCDC\
       cbValueName = MAX_VALUE_NAME
       dwcClassLen = MAX_PATH
       hKeyRoot = HKEY_CURRENT_USER !HKEY_CLASSES_ROOT
 
-!First step is to get version number of Mogul
+! Need to get latest version number of Mogul
       RegPath = 'SOFTWARE\CCDC\'//CHAR(0)
-! Use RegOpenKeyEx() with the new Registry path to get an open handle
-! to the child key you want to enumerate.
+! Will be more than one "latest version" in SOFTWARE CCDC (CSD for instance)
+! so need to get number of values of keys to search through (dwcValues)
+
       retCode = RegOpenKeyEx(hKeyRoot,    &
                              RegPath,     &
                              0,           &
                              KEY_EXECUTE, &
                              LOC(hKey))
-      IF (retCode .NE. ERROR_SUCCESS) GOTO 20
+      IF (retCode .NE. ERROR_SUCCESS) GOTO 30
 ! ADD A QUERY AND ALLOCATE A BUFFER FOR BDATA.
       retCode = RegQueryInfoKey(hKey,                 &  ! Key handle returned from RegOpenKeyEx.
                                 ClassName,            &  ! Buffer for class name.
@@ -333,10 +364,37 @@
                                 LOC(dwcMaxValueData), &  ! Longest Value data.
                                 LOC(dwcSecDesc),      &  ! Security descriptor.
                                 ftLastWriteTime)         ! Last write time.
-      IF (retCode .NE. ERROR_SUCCESS) GOTO 20
+      i = RegCloseKey(hKey) ! Close the key handle.
+
+
+! Look through values of key
+      DO J = 1, dwcValues
+! Use RegOpenKeyEx() with the new Registry path to get an open handle
+! to the child key you want to enumerate.
+      retCode = RegOpenKeyEx(hKeyRoot,    &
+                             RegPath,     &
+                             0,           &
+                             KEY_EXECUTE, &
+                             LOC(hKey))
+      IF (retCode .NE. ERROR_SUCCESS) GOTO 30
+! ADD A QUERY AND ALLOCATE A BUFFER FOR BDATA.
+      retCode = RegQueryInfoKey(hKey,                 &  ! Key handle returned from RegOpenKeyEx.
+                                ClassName,            &  ! Buffer for class name.
+                                LOC(dwcClassLen),     &  ! Length of class string.
+                                NULL,                 &  ! Reserved.
+                                LOC(dwcSubKeys),      &  ! Number of sub keys.
+                                LOC(dwcMaxSubKey),    &  ! Longest sub key size.
+                                LOC(dwcMaxClass),     &  ! Longest class string.
+                                LOC(dwcValues),       &  ! Number of values for this key.
+                                LOC(dwcMaxValueName), &  ! Longest Value name.
+                                LOC(dwcMaxValueData), &  ! Longest Value data.
+                                LOC(dwcSecDesc),      &  ! Security descriptor.
+                                ftLastWriteTime)         ! Last write time.
+      IF (retCode .NE. ERROR_SUCCESS) GOTO 30
       cbData = dwcMaxValueData
 ! ENUMERATE THE KEY.
-      dwLBIndex = 0 ! I'm guessing here ! assuming only one value in directory
+      dwLBIndex = J - 1
+      cbValueName = MAX_VALUE_NAME
       retCode = RegEnumValue (hKey,             & ! Key handle returned from RegOpenKeyEx.
                               dwLBIndex,        & ! Value index, taken from listbox.
                               ValueName,        & ! Name of value.
@@ -345,24 +403,26 @@
                               LOC(dwType),      & ! Type of data.
                               LOC(bData),       & ! Data buffer.
                               LOC(cbData))        ! Size of data buffer.
-      IF (retCode .NE. ERROR_SUCCESS) GOTO 20
-      IF (dwType .NE. REG_SZ) GOTO 20
+      IF (retCode .NE. ERROR_SUCCESS) GOTO 30
+      IF (dwType .NE. REG_SZ) GOTO 30
+
+      IF (ValueName(1:5) .EQ. 'Mogul') THEN
+        LatestVersion = TRIM(bData)
+        RegPath = 'SOFTWARE\CCDC\Mogul\'//LatestVersion(1:cbData)//'\'//CHAR(0) ! this seems to work//'\'//CHAR(0)
+        EXIT
+      ENDIF
       i = RegCloseKey(hKey) ! Close the key handle.
+      ENDDO
 
-! latest mogul version number in bdata
-      LatestVersion = TRIM(Bdata)
-
-      RegPath = 'SOFTWARE\CCDC\Mogul\'//LatestVersion(1:3)//'\'//CHAR(0) ! this seems to work//'\'//CHAR(0)
-
-
-!Get number of values to look through for Mogul exe
+!Looking for executable but will more than one directory listed (install, data etc)
+!so need to find number of values for key
 
       retCode = RegOpenKeyEx(hKeyRoot,    &
                              RegPath,     &
                              0,           &
                              KEY_EXECUTE, &
                              LOC(hKey))
-      IF (retCode .NE. ERROR_SUCCESS) GOTO 20
+      IF (retCode .NE. ERROR_SUCCESS) GOTO 30
 ! ADD A QUERY AND ALLOCATE A BUFFER FOR BDATA.
       retCode = RegQueryInfoKey(hKey,                 &  ! Key handle returned from RegOpenKeyEx.
                                 ClassName,            &  ! Buffer for class name.
@@ -386,7 +446,7 @@
                              0,           &
                              KEY_EXECUTE, &
                              LOC(hKey))
-      IF (retCode .NE. ERROR_SUCCESS) GOTO 20
+      IF (retCode .NE. ERROR_SUCCESS) GOTO 30
 ! ADD A QUERY AND ALLOCATE A BUFFER FOR BDATA.
       retCode = RegQueryInfoKey(hKey,                 &  ! Key handle returned from RegOpenKeyEx.
                                 ClassName,            &  ! Buffer for class name.
@@ -400,7 +460,7 @@
                                 LOC(dwcMaxValueData), &  ! Longest Value data.
                                 LOC(dwcSecDesc),      &  ! Security descriptor.
                                 ftLastWriteTime)         ! Last write time.
-      IF (retCode .NE. ERROR_SUCCESS) GOTO 20
+      IF (retCode .NE. ERROR_SUCCESS) GOTO 30
       cbData = dwcMaxValueData
 
 ! ENUMERATE THE KEY.
@@ -414,21 +474,16 @@
                               LOC(dwType),      & ! Type of data.
                               LOC(bData),       & ! Data buffer.
                               LOC(cbData))        ! Size of data buffer.
-      IF (retCode .NE. ERROR_SUCCESS) GOTO 20
-      IF (dwType .NE. REG_SZ) GOTO 20
-
+      IF (retCode .NE. ERROR_SUCCESS) GOTO 30
+      IF (dwType .NE. REG_SZ) GOTO 30
       TempValueName = ValueName
       IF (TempValueName(1:3) .EQ. 'Exe') THEN
-
         MOGULEXE(1:cbData) = bData(1:cbData)
         RETURN
       ENDIF
       i = RegCloseKey(hKey) ! Close the key handle.
       ENDDO
-
-
-
-   20 CONTINUE
+   30 CONTINUE  ! failure
       i = RegCloseKey(hKey) ! Close the key handle.
 
       END SUBROUTINE GetPathToMogulFromRegistry
