93b583683b58e7f13e9b309b492685c3bd79e19e
Jason Cole <cole@ccdc.cam.ac.uk>
2012-09-07 16:04:05 +0100

    Altered to add in support for direct Japanese e-mailing

--- dash_license_emailer/dash_license_creator.py
+++ dash_license_emailer/dash_license_creator.py
@@ -53,15 +53,20 @@ def cell_to_string(value):
 
 class LicenseDatum:
     def __init__(self, sheet, row):
-
-        v = sheet.cell(row,6).value
-        if type(v) == types.FloatType:
-            self.host_id = str(int(v))
-        else:
-            self.host_id = v.encode('utf8')
+        if sheet != None and row != None:
+            v = sheet.cell(row,6).value
+            if type(v) == types.FloatType:
+                self.host_id = str(int(v))
+            else:
+                self.host_id = v.encode('utf8')
         
-        self.version = cell_to_string(sheet.cell(row,7).value)
-        self.expiry  = cell_to_string(sheet.cell(row,8).value)
+            self.version = cell_to_string(sheet.cell(row,7).value)
+            self.expiry  = cell_to_string(sheet.cell(row,8).value)
+        else:
+            self.host_id = "ABCDABCD"
+            self.version = "3.2"
+            self.expiry = "30/9/2011"
+        
         self.key     = "Unassigned"
         
     def __repr__(self):
@@ -69,18 +74,31 @@ class LicenseDatum:
 
          
 class Organisation:
-    def __init__(self, sheet, row):
-        self.code       = str(sheet.cell(row,0).value)
-        self.agreement_number       = sheet.cell(row,1).value.encode('utf8')
-        self.country                = sheet.cell(row,2).value.encode('utf8')
-        self.organisation           = sheet.cell(row,3).value.encode('utf8')
-        self.type                   = sheet.cell(row,4).value.encode('utf8')
-        self.nlic                   = sheet.cell(row,5).value
-        self.licence_data           = [ LicenseDatum(sheet,row) ]
-        self.site_contact_title     = sheet.cell(row,9).value.encode('utf8')
-        self.site_contact_firstname = sheet.cell(row,10).value.encode('utf8')
-        self.site_contact_surname   = sheet.cell(row,11).value.encode('utf8')
-        self.email                  = sheet.cell(row,12).value.encode('utf8')
+    def __init__(self, sheet = None, row = None):
+        if sheet != None and row != None:
+            self.code       = str(sheet.cell(row,0).value)
+            self.agreement_number       = sheet.cell(row,1).value.encode('utf8')
+            self.country                = sheet.cell(row,2).value.encode('utf8')
+            self.organisation           = sheet.cell(row,3).value.encode('utf8')
+            self.type                   = sheet.cell(row,4).value.encode('utf8')
+            self.nlic                   = sheet.cell(row,5).value
+            self.licence_data           = [ LicenseDatum(sheet,row) ]
+            self.site_contact_title     = sheet.cell(row,9).value.encode('utf8')
+            self.site_contact_firstname = sheet.cell(row,10).value.encode('utf8')
+            self.site_contact_surname   = sheet.cell(row,11).value.encode('utf8')
+            self.email                  = sheet.cell(row,12).value.encode('utf8')
+        else:
+            self.code       = "1111"
+            self.agreement_number       = "D/0000/0000"
+            self.country                = "Outer Mongolia"
+            self.organisation           = "Mickey Mouse University"
+            self.type                   = "Academic"
+            self.nlic                   = 10
+            self.licence_data           = [ LicenseDatum(sheet,row) ]
+            self.site_contact_title     = "Dr"
+            self.site_contact_firstname = "Bob"
+            self.site_contact_surname   = "Monkey"
+            self.email                  = test_email_account
         
     def add_row(self, sheet,row):
         self.licence_data.append(LicenseDatum(sheet,row))
@@ -297,14 +315,54 @@ def generate_licences_implementation(organisations):
             licence.key = licence_key
             time.sleep(0.5)
 
-def write_email(org, out, do_send = 1):
-    draft = email.message_from_string("")
+            
+def write_email_japan(org, out):
+    
+    import codecs
+    f = codecs.open("japan_template_header.unic",encoding='utf-16')
+    body_text = u""
+    for l in f:
+        body_text += l
+
+    body_text += u"\n\nDear Customer (Organisation: %s, Agreement:  %s)" % (org.organisation,org.agreement_number)
+    body_text += u"""
+ 
+Attached key(s) are valid until 30 September 2013. 
+These key(s) are for: DASH 3.0 - 3.2.   
+
+For CSD users: These keys will not work for DASH 3.3 which is distributed with the CSD system.
+
+"""
+    for licence in org.licence_data:
+        if licence.key != None:
+            body_text += "     Key (for version %s) for " % (licence.version)
+
+            if len(licence.host_id) == 4:
+                body_text += "Site Id "
+            else:
+                body_text += "Host Id "
+
+            body_text += "%s: %s \n" % (licence.host_id,licence.key)
+
+    f = codecs.open("japan_template_footer.unic",encoding='utf-16')
+    body_text += u"\n\n"
+    for l in f:
+        body_text += l
+
+    me = 'admin@ccdc.cam.ac.uk'
+    you = get_email(org)
+    
+    return create_email_draft(me,you,'DASH Licence Keys for the next calendar year',body_text)
+    
+
+def write_email_row(org, out):
+    #draft = email.message_from_string("")
     title = "Dr"
     if len(org.site_contact_title) > 0:
         title = org.site_contact_title
 
     body_text = "Dear " + title + " " + org.site_contact_surname +",\n\n"
-    body_text +="Here are your DASH license key(s) for 2012:\n\n"
+    body_text +="Here are your DASH license key(s) for 2013:\n\n"
     for licence in org.licence_data:
         if licence.key != None:
             body_text += "     DASH Key (for version %s) for " % (licence.version)
@@ -316,20 +374,86 @@ def write_email(org, out, do_send = 1):
 
             body_text += "%s: %s \n" % (licence.host_id,licence.key)
 
-    body_text += "\nThis licence key expires on 30 September 2012. Before the licence expires we will send a replacement\n"
-    body_text += "\n\nBest Wishes\n\nLauren Thomas\nCCDC"
+    body_text += "\nThis licence key expires on 30 September 2013. Before the licence expires we will send a replacement\n"
+    body_text += "\n\nBest Wishes\n\nLaura Petley\nCCDC"
 
     me = 'admin@ccdc.cam.ac.uk'
     you = get_email(org)
     
-    draft.set_unixfrom('admin')
-    draft['Subject'] = 'DASH Licence Keys for the next calendar year'
-    draft['From'] = me
-    draft['To'] = you
-    draft.set_payload(body_text)
-    out.write( draft.as_string() +"\n")
+    #draft.set_unixfrom('admin')
+    #draft['Subject'] = 'DASH Licence Keys for the next calendar year'
+    #draft['From'] = me
+    #draft['To'] = you
+    #draft.set_payload(body_text)
+    
+    
+    return create_email_draft(me,you,'DASH Licence Keys for the next calendar year',body_text)
+    
+def create_email_draft(sender, recipient, subject, body):
+    from smtplib import SMTP
+    from email.MIMEText import MIMEText
+    from email.Header import Header
+    from email.Utils import parseaddr, formataddr
+
+    """create an email.
+
+    All arguments should be Unicode strings (plain ASCII works as well).
+
+    Only the real name part of sender and recipient addresses may contain
+    non-ASCII characters.
+
+    The email will be properly MIME encoded and delivered though SMTP to
+    localhost port 25.  This is easy to change if you want something different.
+
+    The charset of the email will be the first one out of US-ASCII, ISO-8859-1
+    and UTF-8 that can represent all the characters occurring in the email.
+    """
+
+    # Header class is smart enough to try US-ASCII, then the charset we
+    # provide, then fall back to UTF-8.
+    header_charset = 'ISO-8859-1'
+
+    # We must choose the body charset manually
+    for body_charset in 'US-ASCII', 'ISO-8859-1', 'UTF-8':
+        try:
+            body.encode(body_charset)
+        except UnicodeError:
+            pass
+        else:
+            break
+
+    # Split real name (which is optional) and email address parts
+    sender_name, sender_addr = parseaddr(sender)
+    recipient_name, recipient_addr = parseaddr(recipient)
+
+    # We must always pass Unicode strings to Header, otherwise it will
+    # use RFC 2047 encoding even on plain ASCII strings.
+    sender_name = str(Header(unicode(sender_name), header_charset))
+    recipient_name = str(Header(unicode(recipient_name), header_charset))
+
+    # Make sure email addresses do not contain non-ASCII characters
+    sender_addr = sender_addr.encode('ascii')
+    recipient_addr = recipient_addr.encode('ascii')
+
+    # Create the message ('plain' stands for Content-Type: text/plain)
+    msg = MIMEText(body.encode(body_charset), 'plain', body_charset)
+    msg['From'] = formataddr((sender_name, sender_addr))
+    msg['To'] = formataddr((recipient_name, recipient_addr))
+    msg['Subject'] = Header(unicode(subject), header_charset)
+    return msg
+
+def write_email( org, out, do_send = 1): 
+    draft = None
+    if org.country == "Japan":
+        draft = write_email_japan(org, out)
+    else:
+        draft = write_email_row(org, out)
+
+    out.write( draft.as_string() +"\n\n\n")
     
     if do_send == 1:
+        you = get_email(org)
+        me = 'admin@ccdc.cam.ac.uk'
         if len(you) > 0:
             # Now - lets send this via SMTP
             s = smtplib.SMTP(smtp_server)
@@ -340,77 +464,79 @@ def write_email(org, out, do_send = 1):
 
 def write_emails(organisations,out):
     for org in organisations:
-        if org.country != "Japan":
-            count = 0
-            for licence in org.licence_data:
-                if licence.key != None: count += 1
+        count = 0
+        for licence in org.licence_data:
+            if licence.key != None: count += 1
 
-            if count > 0:
-                write_email(org,out,1)
+        if count > 0:
+            write_email(org,out,1)
 
-def write_japanese_emails(organisations,out):
-    for org in organisations:
-        if org.country == "Japan":
-            count = 0
-            for licence in org.licence_data:
-                if licence.key != None: count += 1
+#def write_japanese_emails(organisations,out):
+#    for org in organisations:
+#        if org.country == "Japan":
+#            count = 0
+#            for licence in org.licence_data:
+#                if licence.key != None: count += 1
+#
+#            if count > 0:
+#                write_email(org,out,0)
 
-            if count > 0:
-                write_email(org,out,0)
+#def write_japanese_spreadsheet(organisations,out):
+#    for org in organisations:
+#        if len(org.licence_data) > 0 and org.country == "Japan":
+#            out.write(str(org) + "\n")
 
-def write_japanese_spreadsheet(organisations,out):
-    for org in organisations:
-        if len(org.licence_data) > 0 and org.country == "Japan":
-            out.write(str(org) + "\n")
-
-def write_spreadsheet_for_hiromi(organisations,out):
-    
-    def site_line(org):
-        return 'Site licence key is: %s for site id %s' % ( org.licence_data[0].key, org.licence_data[0].host_id)
-        
-    def node_line(org):
-        ret = "Key for "
-        for lic in org.licence_data:
-            v = "%s is %s," % (lic.host_id,lic.key)  
-            ret = ret + v
-
-        # chop off last comma
-        return ret[:-1]
-        
-    out.write('"Agreement#",Org,Name,e-mail,"Site licence or ** installations","S/N and Keys"\n')
-    for org in organisations:
-        if len(org.licence_data) > 0 and org.country == "Japan":
-            if len(org.licence_data) > 1 or len(org.licence_data[0].host_id) > 5:
-                key_line = node_line(org)
-                key_info = str(int(org.nlic)) + " installations"
-            else:
-                key_line = site_line(org)
-                key_info = "unlimited"
-            
- 
-            name =  org.site_contact_title + ' ' + org.site_contact_firstname + ' ' + org.site_contact_surname
-
-            out.write('"%s","%s","%s","%s","%s","%s"\n' % ( org.agreement_number,org.organisation,name,org.email,key_info,key_line ) )
+#def write_spreadsheet_for_hiromi(organisations,out):
+#    
+#    def site_line(org):
+#        return 'Site licence key is: %s for site id %s' % ( org.licence_data[0].key, org.licence_data[0].host_id)
+#        
+#    def node_line(org):
+#        ret = "Key for "
+#        for lic in org.licence_data:
+#            v = "%s is %s," % (lic.host_id,lic.key)  
+#            ret = ret + v
+#
+#        # chop off last comma
+#        return ret[:-1]
+#        
+#    out.write('"Agreement#",Org,Name,e-mail,"Site licence or ** installations","S/N and Keys"\n')
+#    for org in organisations:
+#        if len(org.licence_data) > 0 and org.country == "Japan":
+#            if len(org.licence_data) > 1 or len(org.licence_data[0].host_id) > 5:
+#                key_line = node_line(org)
+#                key_info = str(int(org.nlic)) + " installations"
+#            else:
+#                key_line = site_line(org)
+#                key_info = "unlimited"
+#            
+# 
+#            name =  org.site_contact_title + ' ' + org.site_contact_firstname + ' ' + org.site_contact_surname
+#
+#            out.write('"%s","%s","%s","%s","%s","%s"\n' % ( org.agreement_number,org.organisation,name,org.email,key_info,key_line ) )
             
 
 # Filter out the junk
 
-sys.stderr = open("Errors.txt","w")
+#sys.stderr = open("Errors.txt","w")
 
-raw_organisations = read_worksheet(spreadsheet_name)
+#raw_organisations = read_worksheet(spreadsheet_name)
 
-organisations = prune_organisations(raw_organisations)
+#organisations = prune_organisations(raw_organisations)
 
 # Run the licence generation proces
 # generation process for the pruned organisations
 
-generate_licences(organisations)
+#generate_licences(organisations)
 
 # Write out a csv containing the organisations who are going to get keys
-orgout = open("pruned_organisations.csv","wb")
-save_organisations(organisations,orgout)
+#orgout = open("pruned_organisations.csv","wb")
+#save_organisations(organisations,orgout)
 
 # write out all the emails - currently to a text file and send via jeeves 
+
+organisations = [ Organisation() ]
+
 out = open("emails.txt","wb")
 write_emails(organisations,out)
 
