ed9b4a27e9460d29712b0c809e6000aeb9d3e8a0
Sascha Cunz <scunz@ccdc.cam.ac.uk>
2017-03-13 11:34:21 +0000

    Add file to include dash in build if possible

---
+++ explicit.cmake
@@ -0,0 +1,31 @@
+
+# We can get only here, if we are actually build on Windows and CCDC_WANT_DASH is set, so we can hard code an error
+# if there's no intel fortran compiler:
+
+if(NOT $ENV{CCDC_IFORTRAN_AVAILABLE})
+    message(
+        # FATAL_ERROR
+        STATUS
+        "DASH cannot be compiled without an Intel Fortran compiler"
+    )
+    return()
+endif()
+
+# Compiling DASH only works, if the make-tool is the Microsoft Visual Studio _IDE_!
+# (nmake, jom or ninja based build are not supported)
+#
+# 1. These tools don't know how to build a ".vfproj" file
+# 2. Since the .vfproj contains both debug and release settings, we'd need to have much more information avaiable than
+#    we can get from looking at the current environment variables.
+
+if(NOT MSVC_IDE)
+    message(
+        FATAL_ERROR
+        "Compiling DASH requires to build using the Microsoft Visual Studio IDE. NMake, Jom, Ninja or similar make tools are not supported."
+    )
+endif()
+
+
+set(DASH_BUILD_DIR      "${DASH_BINARY_DIR}/dash.dir")
+set(VFPRO_FILE          "${DASH_BUILD_DIR}/PCDash.vfproj")
+include_external_msproject(${VFPRO_FILE})
