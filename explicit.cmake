7c6898f3f72b6e8c1771482080fd3b83326d2017
Sascha Cunz <scunz@ccdc.cam.ac.uk>
2017-03-15 18:39:53 +0000

    Testing without dash

--- explicit.cmake
+++ explicit.cmake
@@ -1,4 +1,8 @@
 
+# Silently disable this
+return()
+
+
 # We can get only here, if we are actually build on Windows and CCDC_WANT_DASH is set, so we can hard code an error
 # if there's no intel fortran compiler:
 
