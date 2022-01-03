232e2e46b384284a1449c035a71d2706e5b5e535
Claudio Bantaloukas <cbantaloukas@ccdc.cam.ac.uk>
2022-01-03 10:57:00 +0000

    Fetch rc tool NO_JIRA

--- .devcontainer/fetch-winteracter.sh
+++ .devcontainer/fetch-winteracter.sh
@@ -4,7 +4,8 @@ then
     mkdir -p /opt/winteracter
     pushd /opt/winteracter
     curl -H "X-JFrog-Art-Api:$ARTIFACTORY_API_KEY" -LO "https://artifactory.ccdc.cam.ac.uk/artifactory/dash-private-dependencies/winteracter-linux-14.10d.tar.bz2"
-    tar jxf winteracter-linux-14.10d.tar.bz2
     curl -H "X-JFrog-Art-Api:$ARTIFACTORY_API_KEY" -LO "https://artifactory.ccdc.cam.ac.uk/artifactory/dash-private-dependencies/xwint_rc_14_10e.tar.gz"
-    tar zxf xwint_rc_14_10e.tar.gz
+    tar jxf winteracter-linux-14.10d.tar.bz2
+    pushd winteracter-linux-14.10d
+    tar zxf ../xwint_rc_14_10e.tar.gz
 fi
