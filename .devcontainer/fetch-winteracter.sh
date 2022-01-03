336d7e3a167a106853c1b59e10513c50c3daf926
Claudio Bantaloukas <cbantaloukas@ccdc.cam.ac.uk>
2022-01-03 09:35:58 +0000

    Add patched rc from winteracter NO_JIRA

--- .devcontainer/fetch-winteracter.sh
+++ .devcontainer/fetch-winteracter.sh
@@ -3,6 +3,8 @@ then
     echo "Fething winteracter from repository"
     mkdir -p /opt/winteracter
     pushd /opt/winteracter
-    curl -H "X-JFrog-Art-Api:$ARTIFACTORY_API_KEY" -LO "https://artifactory.ccdc.cam.ac.uk/artifactory/ccdc-legacy-windows-distributions/winteracter-linux-14.10d.tar.bz2"
+    curl -H "X-JFrog-Art-Api:$ARTIFACTORY_API_KEY" -LO "https://artifactory.ccdc.cam.ac.uk/artifactory/dash-private-dependencies/winteracter-linux-14.10d.tar.bz2"
     tar jxf winteracter-linux-14.10d.tar.bz2
+    curl -H "X-JFrog-Art-Api:$ARTIFACTORY_API_KEY" -LO "https://artifactory.ccdc.cam.ac.uk/artifactory/dash-private-dependencies/xwint_rc_14_10e.tar.gz"
+    tar zxf xwint_rc_14_10e.tar.gz
 fi
